0558748051fd3109f1463d22dd0d5399bcd37290
Israel Dryer <israel.dryer@gmail.com>
2021-03-21 14:10:55 -0400

    add themes

--- demo.py
+++ demo.py
@@ -1,120 +1,162 @@
+"""
+    An application to demonstrate the available installed themes for TTK
+
+    Author:
+        Israel Dryer
+
+    Modfied:
+        March 21, 2021
+
+    Themes available:
+        - flatly
+        - minty
+        - litera
+        - cosmo
+        - lumen
+        - simplex
+        - sandstone
+        - yeti
+        - pulse
+"""
 from izzythemes import Style, ttk
 import tkinter as tk
 
-# minty, flatly, litera, cosmo, lumen, simplex
 
-def checked(btn):
-    btn_text = "Unchecked" if btn['text'] == "Checked" else "Checked"
-    btn.configure(text=btn_text)
+class Demo(Style):
+    """An application class for demonstrating styles"""
+    def __init__(self):
+        super().__init__()
+        self.theme_use('pulse')
+        self.root = self.master
+        self.root.title('Izzy Themes')
+        self.root.geometry('600x675')
+        self.theme_name = tk.StringVar()
+        self.theme_name.set(self.theme_use())
+        self.setup()
+
+    def setup(self):
+        ttk.Scrollbar(self.root).pack(side='right', fill='y')
+        self.nb = ttk.Notebook(self.root)
+        self.nb.pack(fill='both', expand='yes')
+        tab = self.create_themed_tab()
+        self.nb.add(tab, text='Tab 1')
+        self.nb.add(ttk.Frame(self.nb), text='Tab 2')
+        self.nb.add(ttk.Frame(self.nb), text='Tab 3')
+
+    def change_theme(self, new_theme):
+        self.theme_use(new_theme)
+        self.theme_name.set(new_theme)
+
+    def checked(self, btn):
+        btn_text = "Unchecked" if btn['text'] == "Checked" else "Checked"
+        btn.configure(text=btn_text)
+
+    def create_themed_tab(self):
+        """Create a return a frame containing themed widgets"""
+        tab = ttk.Frame(self.nb, padding=20)
+        colors = ['Primary', 'Secondary', 'Success', 'Info', 'Warning', 'Danger']
+
+        header_frame = ttk.Frame(tab)
+        header = ttk.Label(header_frame, textvariable=self.theme_name, font='-size 30')
+        header.pack(side='left', fill='x', pady=5)
+        header_frame.pack(fill='x')
+
+        # Menubutton (select a theme)
+        mb = ttk.Menubutton(header_frame, text='Select a theme to preview')
+        mb.pack(side='right', fill='x', padx=(0, 5), pady=5)
+        mb.menu = tk.Menu(mb)
+        mb['menu'] = mb.menu
+        for t in self.theme_names():
+            mb.menu.add_command(label=t, command=lambda theme_name=t: self.change_theme(theme_name))
+
+        # Available Colors
+        color_frame = ttk.Labelframe(tab, text='Colors available in this theme', padding=15)
+        for color in colors:
+            ttk.Button(color_frame, text=color.title(), style=f'{color.lower()}.TButton').pack(side='left', fill='x', expand='yes', padx=2)
+        color_frame.pack(side='top', fill='x', pady=5)
+
+        # Widget examples
+        widget_frame = ttk.Labelframe(tab, padding=15, text='Widget examples')
+        widget_frame.pack(fill='x', expand='yes')
+
+        # Label
+        ttk.Label(widget_frame, text='This is a label').pack(side='top', fill='x', pady=5)
+
+        entry_spin_frame = ttk.Frame(widget_frame)
+        entry_spin_frame.pack(fill='x', pady=5)
+
+        # Entry
+        entry = ttk.Entry(entry_spin_frame)
+        entry.insert('end', 'An entry field with focus ring')
+        entry.pack(side='left', fill='x', expand='yes')
+
+        # Spinbox
+        spinner_options = ['Spinner option 1', 'Spinner option 2', 'Spinner option 3']
+        spinner = ttk.Spinbox(entry_spin_frame, values=spinner_options)
+        spinner.set('Spinner option 1')
+        spinner.pack(side='left', fill='x', expand='yes', padx=(5, 0))
+
+        # Button
+        btn_frame = ttk.Frame(widget_frame)
+        b1 = ttk.Button(btn_frame, text='Solid Button')
+        b1.pack(side='left', fill='x', expand='yes', padx=(0, 5))
+        b2 = ttk.Button(btn_frame, text='Outline Button', style='Outline.TButton')
+        b2.pack(side='left', fill='x', expand='yes')
+        btn_frame.pack(fill='x', pady=5)
+
+        # Labelframe
+        options_frame = ttk.Frame(widget_frame)
+        options_frame.pack(fill='x', pady=5)
+
+        # Radio
+        r1 = ttk.Radiobutton(options_frame, value=1, text='Radio one')
+        r1.pack(side='left', fill='x', expand='yes')
+        r1.invoke()
+        r2 = ttk.Radiobutton(options_frame, value=2, text='Radio two')
+        r2.pack(side='left', fill='x', expand='yes')
+
+        # Checkbutton
+        cb1 = ttk.Checkbutton(options_frame, text='Unchecked')
+        cb1.configure(command=lambda: self.checked(cb1))
+        cb1.pack(side='left', fill='x', expand='yes')
+        cb1.invoke()
+
+        cb2 = ttk.Checkbutton(options_frame, text='Unchecked')
+        cb2.configure(command=lambda: self.checked(cb2))
+        cb2.pack(side='left', fill='x', expand='yes')
+
+        # Treeview
+        tv = ttk.Treeview(widget_frame, height=3)
+        tv.pack(fill='x', pady=5)
+        tv.heading('#0', text='Example heading')
+        tv.insert('', 'end', 'example1', text='Example 1')
+        tv.insert('', 'end', 'example2', text='Example 2')
+        tv.insert('example2', 'end', text='Example 2 Child 1')
+        tv.insert('example2', 'end', text='Example 2 Child 2')
+
+        # Scale
+        scale_frame = ttk.Frame(widget_frame)
+        scale_var = tk.IntVar(value=25)
+        scale = ttk.Scale(scale_frame, variable=scale_var, from_=1, to=100)
+        scale.pack(side='left', fill='x', expand='yes', padx=(0, 2))
+        scale_frame.pack(side='top', fill='x', pady=5)
+        entry = ttk.Entry(scale_frame, textvariable=scale_var, width=4)
+        entry.pack(side='right')
+
+        # Combobox
+        cbo = ttk.Combobox(widget_frame, values=colors)
+        cbo.current(0)
+        cbo.pack(fill='x', pady=5)
+
+        # Progressbar
+        ttk.Progressbar(widget_frame, value=30).pack(fill='x', pady=5)
+        return tab
+
+    def run(self):
+        self.root.mainloop()
 
 
-style = Style()
-style.theme_use('simplex')
-root = style.master
-root.title('IzzyThemes')
-root.geometry('500x500')
+if __name__ == '__main__':
 
-# Scrollbar
-ttk.Scrollbar(root).pack(side='right', fill='y')
-
-# Notebook
-nb = ttk.Notebook(root)
-nb.pack(fill='both', expand='yes')
-
-
-def create_themed_tab(color=''):
-    """Create a return a frame containing themed widgets"""
-    style_color = color + '.' if color else color
-    print(style_color)
-    tab = ttk.Frame(nb, padding=5)
-    colors = ['Primary', 'Secondary', 'Success', 'Info', 'Warning', 'Danger']
-
-    # Label
-    ttk.Label(tab, text='This is an example label', style=f'{style_color}TLabel').pack(side='top', fill='x', pady=5)
-
-    frm2 = ttk.Frame(tab)
-    frm2.pack(fill='x', pady=5)
-
-    # Menubutton
-    mb = ttk.Menubutton(frm2, text='Menubutton', style=f'{style_color}TMenubutton')
-    mb.pack(side='left', fill='x', padx=(0, 5))
-    mb.menu = tk.Menu(mb)
-    mb['menu'] = mb.menu
-    for color in colors:
-        mb.menu.add_radiobutton(label=color)
-
-    # Entry
-    ttk.Entry(frm2, style=f'{style_color}TEntry').pack(side='left', fill='x')
-
-    # Spinbox
-    ttk.Spinbox(frm2, from_=1, to=25, style=f'{style_color}TSpinbox').pack(side='left', padx=(5, 0))
-
-    # Button
-    btn_frame = ttk.Frame(tab)
-    b1 = ttk.Button(btn_frame, text='Button', style=f'{style_color}TButton')
-    b1.pack(side='left', fill='x', expand='yes', padx=(0, 5))
-    b2 = ttk.Button(btn_frame, text='Button', style=f'{style_color}Outline.TButton')
-    b2.pack(side='left', fill='x', expand='yes')
-    btn_frame.pack(fill='x', pady=5)
-
-    # Labelframe
-    lf = ttk.Labelframe(tab, text='Labelframe', style=f'{style_color}TLabelframe')
-    lf.pack(fill='x', pady=5)
-
-    # Radio
-    radio_frame = ttk.Frame(lf)
-    radio_frame.pack(side='top', fill='x')
-    r1 = ttk.Radiobutton(radio_frame, value=1, text='Radio one', width=15, style=f'{style_color}TRadiobutton')
-    r1.pack(side='left', fill='x')
-    r1.invoke()
-    ttk.Radiobutton(radio_frame, value=2, text='Radio two', style=f'{style_color}TRadiobutton').pack(side='left',
-                                                                                                     fill='x')
-
-    # Checkbutton
-    check_frame = ttk.Frame(lf)
-    check_frame.pack(side='top', fill='x')
-    cb1 = ttk.Checkbutton(check_frame, text='Unchecked', width=15, style=f'{style_color}TCheckbutton')
-    cb1.configure(command=lambda: checked(cb1))
-    cb1.pack(side='left')
-    cb1.invoke()
-
-    cb2 = ttk.Checkbutton(check_frame, text='Unchecked', style=f'{style_color}TCheckbutton')
-    cb2.configure(command=lambda: checked(cb2))
-    cb2.pack(side='left')
-
-    # Treeview
-    tv = ttk.Treeview(tab, height=3, style=f'{style_color}Treeview')
-    tv.pack(fill='x', pady=5)
-    tv.heading('#0', text='Example heading')
-    tv.insert('', 'end', 'example1', text='Example 1')
-    tv.insert('', 'end', 'example2', text='Example 2')
-    tv.insert('example2', 'end', text='Example 2 Child 1')
-    tv.insert('example2', 'end', text='Example 2 Child 2')
-
-    # Scale
-    scale_frame = ttk.Frame(tab)
-    scale_var = tk.IntVar(value=25)
-    scale = ttk.Scale(scale_frame, variable=scale_var, from_=1, to=100, style=f'{style_color}Horizontal.TScale')
-    scale.pack(side='left', fill='x', expand='yes', padx=(0, 2))
-    scale_frame.pack(side='top', fill='x', pady=5)
-    ttk.Entry(scale_frame, textvariable=scale_var, width=4, style=f'{style_color}TEntry').pack(side='right')
-
-    # Combobox
-    cbo = ttk.Combobox(tab, values=colors, style=f'{style_color}TCombobox')
-    cbo.current(0)
-    cbo.pack(fill='x', pady=5)
-
-    # Progressbar
-    ttk.Progressbar(tab, value=30, style=f'{style_color}Horizontal.TProgressbar').pack(fill='x', pady=5)
-
-    return tab
-
-
-nb.add(create_themed_tab(''), text='Primary')
-nb.add(create_themed_tab('secondary'), text='Secondary')
-nb.add(create_themed_tab('info'), text='Info')
-nb.add(create_themed_tab('success'), text='Success')
-nb.add(create_themed_tab('warning'), text='Warning')
-nb.add(create_themed_tab('danger'), text='Danger')
-
-root.mainloop()
+    demo = Demo()
+    demo.run()
--- izzythemes.py
+++ izzythemes.py
@@ -1,18 +1,38 @@
 from theme import Theme, Colors, ttk
 
-# TODO the fg needs to be the same color as the main font folor
-# TODO the input-fg needs to be a % lighter than the fg color
-# TODO consider adding an input background color... which may always be white?
 
 class Style(ttk.Style):
     def __init__(self):
         super().__init__()
-        Theme(self, *FLATLY)
-        Theme(self, *MINTY)
-        Theme(self, *LITERA)
-        Theme(self, *COSMO)
-        Theme(self, *LUMEN)
-        Theme(self, *SIMPLEX)
+        self.flatly = Theme(self, *FLATLY)
+        self.minty = Theme(self, *MINTY)
+        self.litera = Theme(self, *LITERA)
+        self.cosmo = Theme(self, *COSMO)
+        self.lumen = Theme(self, *LUMEN)
+        self.simplex = Theme(self, *SIMPLEX)
+        self.sandstone = Theme(self, *SANDSTONE)
+        self.yeti = Theme(self, *YETI)
+        self.pulse = Theme(self, *PULSE)
+
+
+    def theme_use(self, themename=None):
+        """If themename is None, returns the theme in use, otherwise, set
+        the current theme to themename, refreshes all widgets and emits
+        a <<ThemeChanged>> event."""
+        if themename is None:
+            # Starting on Tk 8.6, checking this global is no longer needed
+            # since it allows doing self.tk.call(self._name, "theme", "use")
+            return self.tk.eval("return $ttk::currentTheme")
+
+        # Set the global style settings for non-tk widgets
+        theme_obj = self.__dict__.get(themename)
+        if theme_obj:
+            theme_obj.apply_global_tk_styles()
+
+        # using "ttk::setTheme" instead of "ttk::style theme use" causes
+        # the variable currentTheme to be updated, also, ttk::setTheme calls
+        # "ttk::style theme use" in order to change theme.
+        self.tk.call("ttk::setTheme", themename)
 
 
 FLATLY = ('flatly',
@@ -31,25 +51,25 @@ FLATLY = ('flatly',
               dark='#888888',
               active='#dadada',
               border='#ced4da'
-          ), 'Roboto')
+          ), 'Helvetica')
 
 MINTY = ('minty',
-          Colors(
-              primary='#78c2ad',
-              secondary='#f3969a',
-              success='#56cc9d',
-              info='#6cc3d5',
-              warning='#ffce67',
-              danger='#ff7851',
-              bg='#ffffff',
-              fg='#888888',
-              selectbg='#6cc3d5',
-              selectfg='#ffffff',
-              light='#f8f9fa',
-              dark='#888888',
-              active='#dadada',
-              border='#ced4da'
-          ), 'Roboto')
+         Colors(
+             primary='#78c2ad',
+             secondary='#f3969a',
+             success='#56cc9d',
+             info='#6cc3d5',
+             warning='#ffce67',
+             danger='#ff7851',
+             bg='#ffffff',
+             fg='#5a5a5a',
+             selectbg='#6cc3d5',
+             selectfg='#ffffff',
+             light='#f8f9fa',
+             dark='#5a5a5a',
+             active='#dadada',
+             border='#ced4da'
+         ), 'Helvetica')
 
 LITERA = ('litera',
           Colors(
@@ -67,58 +87,116 @@ LITERA = ('litera',
               dark='#343a40',
               active='#dadada',
               border='#e5e5e5'
-          ), 'Roboto')
+          ), 'Helvetica')
 
 COSMO = ('cosmo',
-          Colors(
-              primary='#2780e3',
-              secondary='#373a3c',
-              success='#3fb618',
-              info='#9954bb',
-              warning='#ff7518',
-              danger='#ff0039',
-              bg='#ffffff',
-              fg='#c7c8c8',
-              selectbg='#3fb618',
-              selectfg='#ffffff',
-              light='#fdfdfe',
-              dark='#c7c8c8',
-              active='#dadada',
-              border='#ced4da'
-          ), 'Roboto')
+         Colors(
+             primary='#2780e3',
+             secondary='#373a3c',
+             success='#3fb618',
+             info='#9954bb',
+             warning='#ff7518',
+             danger='#ff0039',
+             bg='#ffffff',
+             fg='#c7c8c8',
+             selectbg='#3fb618',
+             selectfg='#ffffff',
+             light='#fdfdfe',
+             dark='#c7c8c8',
+             active='#dadada',
+             border='#ced4da'
+         ), 'Helvetica')
 
 LUMEN = ('lumen',
-          Colors(
-              primary='#158cba',
-              secondary='#e3e3e3',
-              success='#28b62c',
-              info='#75caeb',
-              warning='#ff851b',
-              danger='#ff4136',
-              bg='#ffffff',
-              fg='#555555',
-              selectbg='#75caeb',
-              selectfg='#ffffff',
-              light='#f6f6f6',
-              dark='#555555',
-              active='#dadada',
-              border='#ced4da'
-          ), 'Roboto')
+         Colors(
+             primary='#158cba',
+             secondary='#555555',
+             success='#28b62c',
+             info='#75caeb',
+             warning='#ff851b',
+             danger='#ff4136',
+             bg='#ffffff',
+             fg='#555555',
+             selectbg='#75caeb',
+             selectfg='#ffffff',
+             light='#f6f6f6',
+             dark='#555555',
+             active='#dadada',
+             border='#ced4da'
+         ), 'Helvetica')
 
 SIMPLEX = ('simplex',
-          Colors(
-              primary='#d7220e',
-              secondary='#f8f8f8',
-              success='#459307',
-              info='#0282ae',
-              warning='#cd7c1d',
-              danger='#9a469e',
-              bg='#fcfcfc',
-              fg='#212529',
-              selectbg='#0282ae',
-              selectfg='#ffffff',
-              light='#ffffff',
-              dark='#cccccc',
-              active='#d8d8d8',
-              border='#bbbbbb'
-          ), 'Roboto')
+           Colors(
+               primary='#d7220e',
+               secondary='#373a3c',
+               success='#459307',
+               info='#0282ae',
+               warning='#cd7c1d',
+               danger='#9a469e',
+               bg='#fcfcfc',
+               fg='#212529',
+               selectbg='#0282ae',
+               selectfg='#ffffff',
+               light='#ffffff',
+               dark='#212529',
+               active='#d8d8d8',
+               border='#bbbbbb'
+           ), 'Helvetica')
+
+
+SANDSTONE = ('sandstone',
+           Colors(
+               primary='#3e3f3a',
+               secondary='#8e8c84',
+               success='#93c54b',
+               info='#29abe0',
+               warning='#f47c3c',
+               danger='#d9534f',
+               bg='#ffffff',
+               fg='#3e3f3a',
+               selectbg='#29abe0',
+               selectfg='#ffffff',
+               light='#fdfcfb',
+               dark='#3e3f3a',
+               active='#d8d8d8',
+               border='#ced4da'
+           ), 'Helvetica')
+
+
+YETI = ('yeti',
+           Colors(
+               primary='#008cba',
+               secondary='#333333',
+               success='#43ac6a',
+               info='#5bc0de',
+               warning='#e99002',
+               danger='#f04124',
+               bg='#ffffff',
+               fg='#222222',
+               selectbg='#5bc0de',
+               selectfg='#ffffff',
+               light='#eeeeee',
+               dark='#222222',
+               active='#dadada',
+               border='#cccccc'
+           ), 'Helvetica')
+
+
+PULSE = ('pulse',
+           Colors(
+               primary='#593196',
+               secondary='#17141f',
+               success='#13b955',
+               info='#009cdc',
+               warning='#efa31d',
+               danger='#fc3939',
+               bg='#ffffff',
+               fg='#444444',
+               selectbg='#009cdc',
+               selectfg='#ffffff',
+               light='#fdfdfe',
+               dark='#444444',
+               active='#dadada',
+               border='#cbc8d0'
+           ), 'Helvetica')
+
