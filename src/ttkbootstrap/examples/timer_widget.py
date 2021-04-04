2bb990dda9ab2c56b4c795e268bb549823bd4886
Israel Dryer <israel.dryer@gmail.com>
2021-04-04 15:13:04 -0400

    update docs and examples

--- src/ttkbootstrap/examples/timer_widget.py
+++ src/ttkbootstrap/examples/timer_widget.py
@@ -1,65 +1,70 @@
-from ttkbootstrap import Style
 import tkinter
 from tkinter import ttk
+from ttkbootstrap import Style
 
 
-def toggle_button():
-    """Toggle between start and paused"""
-    if running.get():
-        pause_timer()
-        running.set(False)
-        toggle_btn.configure(text='Start', style='info.TButton')
-    else:
-        start_timer()
-        running.set(True)
-        toggle_btn.configure(text='Pause', style='info.Outline.TButton')
+class Application(tkinter.Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.title('Timer')
+        self.style = Style()
+        self.timer = TimerWidget(self)
+        self.timer.pack(fill='both', expand='yes')
 
 
-def start_timer():
-    after_id.set(window.after(1, increment_timer))
+class TimerWidget(ttk.Frame):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # variables
+        self.running = tkinter.BooleanVar(value=False)
+        self.after_id = tkinter.StringVar()
+        self.time_elapsed = tkinter.IntVar()
+        self.time_text = tkinter.StringVar(value='00:00:00')
+
+        # timer label
+        self.timer_lbl = ttk.Label(self, font='-size 32', anchor='center', textvariable=self.time_text)
+        self.timer_lbl.pack(side='top', fill='x', padx=60, pady=20)
+
+        # control buttons
+        self.toggle_btn = ttk.Button(self, text='Start', width=10, style='info.TButton', command=self.toggle)
+        self.toggle_btn.pack(side='left', fill='x', expand='yes', padx=10, pady=10)
+
+        self.reset_btn = ttk.Button(self, text='Reset', width=10, style='success.TButton', command=self.reset)
+        self.reset_btn.pack(side='left', fill='x', expand='yes', pady=10)
+
+        self.quit_btn = ttk.Button(self, text='Quit', width=10, style='danger.TButton', command=self.quit)
+        self.quit_btn.pack(side='left', fill='x', expand='yes', padx=10, pady=10)
+
+    def toggle(self):
+        if self.running.get():
+            self.pause()
+            self.running.set(False)
+            self.toggle_btn.configure(text='Start', style='info.TButton')
+        else:
+            self.start()
+            self.running.set(True)
+            self.toggle_btn.configure(text='Pause', style='info.Outline.TButton')
+
+    def pause(self):
+        self.after_cancel(self.after_id.get())
+
+    def start(self):
+        self.after_id.set(self.after(1, self.increment))
+
+    def increment(self):
+        current = self.time_elapsed.get() + 1
+        self.time_elapsed.set(current)
+        time_str = '{:02d}:{:02d}:{:02d}'.format((current // 100) // 60, (current // 100) % 60, current % 100)
+        self.time_text.set(time_str)
+        self.after_id.set(self.after(100, self.increment))
+
+    def reset(self):
+        self.time_elapsed.set(0)
+        self.time_text.set('00:00:00')
 
 
-def pause_timer():
-    window.after_cancel(after_id.get())
-
-
-def increment_timer():
-    """Add one second to timer"""
-    current = seconds_var.get() + 1
-    seconds_var.set(current)
-    time_string = '{:02d}:{:02d}:{:02d}'.format((current // 100) // 60, (current // 100) % 60, current % 100)
-    timer_var.set(time_string)
-    after_id.set(window.after(100, increment_timer))
-
-
-def reset_timer():
-    seconds_var.set(0)
-    timer_var.set('00:00:00')
-
-
-# instantiate the window
-style = Style()
-window = style.master
-window.title('Timer')
-
-# variables
-seconds_var = tkinter.IntVar(value=0)
-timer_var = tkinter.StringVar(value='00:00:00')
-after_id = tkinter.StringVar()
-running = tkinter.BooleanVar(value=False)
-
-# timer label
-timer_lbl = ttk.Label(window, font='-size 32', anchor='center', textvariable=timer_var)
-timer_lbl.pack(side='top', fill='x', padx=60, pady=20)
-
-# control buttons
-toggle_btn = ttk.Button(window, text='Start', width=10, style='info.TButton', command=toggle_button)
-toggle_btn.pack(side='left', fill='x', expand='yes', padx=10, pady=10)
-
-reset_btn = ttk.Button(window, text='Reset', width=10, style='success.TButton', command=reset_timer)
-reset_btn.pack(side='left', fill='x', expand='yes', pady=10)
-
-exit_btn = ttk.Button(window, text='Exit', width=10, style='danger.TButton', command=window.quit)
-exit_btn.pack(side='left', fill='x', expand='yes', padx=10, pady=10)
-
-window.mainloop()
+if __name__ == '__main__':
+    Application().mainloop()
