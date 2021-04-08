7079ff8360ae2d59aac71959f77210de7269f760
Israel Dryer <israel.dryer@gmail.com>
2021-04-08 17:19:21 -0400

    updated settings for collapsing frame

--- src/ttkbootstrap/gallery/collapsing_frame.py
+++ src/ttkbootstrap/gallery/collapsing_frame.py
@@ -18,19 +18,19 @@ class Application(tkinter.Tk):
         cf.pack(fill='both')
 
         # option group 1
-        group1 = ttk.Frame(cf)
+        group1 = ttk.Frame(cf, padding=10)
         for x in range(5):
             ttk.Checkbutton(group1, text=f'Option {x+1}').pack(fill='x')
         cf.add(group1, title='Option Group 1', style='primary.TButton')
 
         # option group 2
-        group2 = ttk.Frame(cf)
+        group2 = ttk.Frame(cf, padding=10)
         for x in range(5):
             ttk.Checkbutton(group2, text=f'Option {x+1}').pack(fill='x')
         cf.add(group2, title='Option Group 2', style='danger.TButton')
 
         # option group 3
-        group3 = ttk.Frame(cf)
+        group3 = ttk.Frame(cf, padding=10)
         for x in range(5):
             ttk.Checkbutton(group3, text=f'Option {x+1}').pack(fill='x')
         cf.add(group3, title='Option Group 3', style='success.TButton')
@@ -49,7 +49,7 @@ class CollapsingFrame(ttk.Frame):
         self.images = [tkinter.PhotoImage(name='open', file='assets/icons8_double_up_24px.png'),
                        tkinter.PhotoImage(name='closed', file='assets/icons8_double_right_24px.png')]
 
-    def add(self, child, title="", style='primary.TButton'):
+    def add(self, child, title="", style='primary.TButton', **kwargs):
         """Add a child to the collapsible frame
 
         :param ttk.Frame child: the child frame to add to the widget
@@ -60,16 +60,22 @@ class CollapsingFrame(ttk.Frame):
             return
         style_color = style.split('.')[0]
         frm = ttk.Frame(self, style=f'{style_color}.TFrame')
-        frm.grid(row=self.cumulative_rows, column=0, sticky='news')
+        frm.grid(row=self.cumulative_rows, column=0, sticky='ew')
+
         # header title
-        lbl = ttk.Label(frm, text=title, style=f'{style_color}.TButton', font=('Helvetica 11 bold'))
-        lbl.pack(side='left', fill='x', pady=10, padx=10)
+        lbl = ttk.Label(frm, text=title, style=f'{style_color}.TButton')
+        if kwargs.get('textvariable'):
+            lbl.configure(textvariable=kwargs.get('textvariable'))
+        lbl.pack(side='left', fill='both', padx=10)
+
         # header toggle button
         btn = ttk.Button(frm, image='open', style=style, command=lambda c=child: self._toggle_open_close(child))
         btn.pack(side='right')
+
         # assign toggle button to child so that it's accesible when toggling (need to change image)
         child.btn = btn
-        child.grid(row=self.cumulative_rows + 1, column=0, sticky='ew', padx=5, pady=5)
+        child.grid(row=self.cumulative_rows + 1, column=0, sticky='news')
+
         # increment the row assignment
         self.cumulative_rows += 2
 
