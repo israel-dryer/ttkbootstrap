be44fa58e149b5271c8f420d174ec0e05930efd8
Israel Dryer <israel.dryer@gmail.com>
2021-04-09 17:28:39 -0400

    update theme images per arrow size changes

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ the ability to easily create your own.
 
 setuptools.setup(
     name="ttkbootstrap",
-    version="0.0.33",
+    version="0.0.37",
     author="Israel Dryer",
     author_email="israel.dryer@gmail.com",
     description="A collection of modern ttk themes inspired by Bootstrap",
--- src/ttkcreator/__init__.py
+++ src/ttkcreator/__init__.py
@@ -313,7 +313,7 @@ class CreatorDesignWindow(tk.Toplevel):
         img = ImageGrab.grab(bbox=bbox)
 
         # image name
-        filename = f'../docs/images/ttkcreator.png'
+        filename = f'docs/images/ttkcreator.png'
         print(filename)
         img.save(filename, 'png')
 
