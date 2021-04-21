eb140250ffff4b1242e745953f389729e1e3b084
daniilS <daniil.soloviev@gmail.com>
2021-04-21 13:20:49 +0100

    Fix Symbola font linking

--- src/ttkbootstrap/__init__.py
+++ src/ttkbootstrap/__init__.py
@@ -953,8 +953,8 @@ class StylerTTK:
         Create assets needed for scrollbar arrows
         """
         font_size = 13
-        fnt = ImageFont.truetype(r'C:\Users\us43060\PycharmProjects\ttk-bootstrap\src\ttkbootstrap\Symbola.ttf',
-                                 font_size)
+        with importlib.resources.open_binary('ttkbootstrap', 'Symbola.ttf') as font_path:
+            fnt = ImageFont.truetype(font_path, font_size)
 
         hsup_im = Image.new('RGBA', (font_size, font_size))
         up_draw = ImageDraw.Draw(hsup_im)
