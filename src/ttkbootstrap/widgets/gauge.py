39e726066311773d3f93861b22b495c191f98106
Israel Dryer <israel.dryer@gmail.com>
2021-04-24 13:33:11 -0400

    added getters and setters for the value and text properties

--- src/ttkbootstrap/widgets/gauge.py
+++ src/ttkbootstrap/widgets/gauge.py
@@ -2,81 +2,95 @@ import tkinter as tk
 from tkinter import ttk
 
 from PIL import Image, ImageTk, ImageDraw
-from ttkbootstrap import Style
+from ttkbootstrap import Style, Colors
 
 
 class RadialGauge(ttk.Label):
     """
     A RadialGauge widget shows the amount completed relative to the total amount of work to be done.
 
-    :param str background: use this option to set the background color. You may also use the ``style``.
-    :param str cursor: use this option to specify the appearance of the mouse cursor when it is over the widget.
-    :param str font: use this option to specify the font style for the displayed text. You man also use the ``style``.
-    :param str foreground: use this option to specify the color of the displayed text. You may also use the ``style``.
-    :param str indicator color: use this option to specify the indicator color. Default is style foreground color.
-    :param str justify: if the text you provide contains newline ``\n`` characters, this option specifies how each line
-        will be positioned horizontally: **left**, **center**, **right**. You may also use the ``style``.
-    :param Union[Iterable[int], int] maximum: the maximum value of the indicator; default is 100.
-    :param any padding: to add more space around all four sides of the gauge, set this option to the desired dimension.
-        You may also use the ``style``.
-    :param str style: the style to be used in rendering the widget.
-    :param object styler: an instance of ``Style``. If not provided, it will be created and the default application
-        theme will be used.
-    :param str theme: the name of theme to use when styling the widget. Use any of the `TLabel` style classes.
-    :param bool takefocus: use this option to specify whether the widget is visited during focus traversal.
-    :param str text: a string of text to be displayed in the widget.
-    :param object textvariable: a ``StringVar`` instance; get and set the text displayed in the widget; if a ``text``
-        option is used, then ``text`` will be ignored. If not provided, it will be created and can be accessed via the
-        ``textvariable`` property.
-    :param str troughcolor: use this option to set the color of the trough: Default is theme border color.
-    :param int underline: You can request that one of the letters in the text string be underlined by setting this
-        option to the position of that letter.
-    :param int value: the current value of the progressbar. If the variable is set to an existing value, this has no
-        effect.
-    :param object variable: an ``IntVar`` instance; get and set the current value of the indicator. If a variable is
-        not provided, one will be created and can be accessed via the ``variable`` property``.
+    :param str background: the widget background color; may also be set with ``style``.
+    :param str cursor: specify the appearance of the mouse cursor when it is over the widget.
+    :param str font: font style for the displayed text; may also be set with ``style``.
+    :param str foreground: the color of the displayed text; may also be set with ``style``.
+    :param str indicatorcolor: the color of the indicator ring; default color is the `primary` theme color.
+    :param int indicatorthickness: indicator thickness in pixels; default is 20.
+    :param str justify: specifies how each line will be positioned horizontally if newline characters are used.
+    :param int maximum: maximum value of the indicator; default is 100.
+    :param any padding: adds space around all four sides of the gauge; may also be set with ``style``.
+    :param str style: the style used to render the widget.
+    :param int startangle: the starting angle of the indicator; default is 90 degrees.
+    :param bool takefocus: enables the widget to be visited during focus traversal.
+    :param str text: a string of text displayed in the widget.
+    :param object textvariable: a ``StringVar`` instance; gets and sets the text of the widget; will be created by
+        default if none is passed; can be accessed via the ``textvariable`` property.
+    :param str troughcolor: the color of the trough; default is the theme input background color.
+    :param str underline: creates an underline under the letters in the text; pass in the index of the letters.
+    :param object variable: an ``IntVar`` instance; get and set the current value of the indicator; if none is passed,
+        one will be created and can be accessed via the ``variable`` property.
 
-    ..note:: Because this is not strictly a `ttk` widget, you need to pass in a reference to a ``Style`` instance with
-        the ``styler`` argument so that the widget knows what theme to extract the colors values from. If not provided,
-        one will be created with the default application theme `flatly`.
     """
 
-    def __init__(self, parent, styler=None, style='primary.TLabel', **kwargs):
-
+    def __init__(self, parent, **kwargs):
         excluded = ['anchor', 'theme', 'image', 'compound', 'relief', 'bordwidth', 'maximum', 'value', 'variable',
-                    'indicatorcolor', 'troughcolor', 'textvariable']
+                    'indicatorcolor', 'troughcolor', 'textvariable', 'angle']
         kwargs_ = {k: v for k, v in kwargs.items() if k not in excluded}
-        super().__init__(parent, class_='RadialGauge', style=style, **kwargs_)
+        self.style = kwargs.get('style') or 'primary.TLabel'
+        super().__init__(parent, class_='RadialGauge', style=self.style, **kwargs_)
 
-        self.styler = Style() if not styler else styler
-        self.indicatorcolor = kwargs.get('indicatorcolor') or self.styler.lookup(style, 'foreground')
-        self.troughcolor = kwargs.get('troughcolor') or self.styler.lookup('Horizontal.TProgressbar', 'troughcolor')
-        self.is_maximum = False  # used to control the forward and reverse on the ``step`` method
+        self.indicatorcolor = kwargs.get('indicatorcolor') or self._lookup(self.style, 'foreground')
+        self.indicatorthickness = kwargs.get('indicatorthickness') or 20
+        self.troughcolor = kwargs.get('troughcolor') or Colors.update_hsv(self._lookup('TCombobox', 'background'),
+                                                                          vd=-0.03)
+        self.size = kwargs.get('size') or 200
+        self.startangle = kwargs.get('angle') or 90
+        self.im = Image.new('RGBA', (self.size, self.size))
+        self.arc = ImageTk.PhotoImage(self.im)
 
-        # variable controls
         self.textvariable = kwargs.get('textvariable') or tk.StringVar(value=kwargs.get('text'))
         self.configure(textvariable=self.textvariable)
         self.variable = kwargs.get('variable') or tk.IntVar(value=kwargs.get('value'))
         self.variable.trace_add('write', self.update_indicator)
-        self.maximum = 100 if kwargs.get('maximum') == None or kwargs.get('maximum') <= 0 else kwargs.get('maximum')
 
-        # create the image for drawing the arc trough and indicator
-        self.arc = None
-        self.size = kwargs.get('size') or 100  # the eventual size of the widget
-        self.im = Image.new('RGBA', (1000, 1000))  # create large and then subsample for higher quality image
-        draw = ImageDraw.Draw(self.im)
-        draw.arc((0, 0, 990, 990), 0, 360, self.troughcolor, 100)
-        self.arc = ImageTk.PhotoImage(self.im.resize((self.size, self.size), Image.CUBIC))
+        self.maximum = 100 if kwargs.get('maximum') == None or kwargs.get('maximum') <= 0 else kwargs.get('maximum')
+        self.is_maximum = False
 
         self.configure(compound='center', anchor='center', image=self.arc)
         self.bind('<Configure>', self.resize)
 
+    @property
+    def text(self):
+        return self.textvariable.get()
+
+    @text.setter
+    def text(self, value):
+        self.textvariable.set(value)
+
+    @property
+    def value(self):
+        return self.variable.get()
+
+    @value.setter
+    def value(self, value):
+        self.variable.set(value)
+
+    def _lookup(self, style, option):
+        """
+        Wrapper around the tcl style lookup command
+
+        :param style: the name of the style used for rendering a widget
+        :param option: the option to lookup from the style option database
+        :return: the value of the option looked up
+        :rtype: str
+        """
+        return self.tk.call("ttk::style", "lookup", style, '-%s' % option, None, None)
+
     def resize(self, event):
         """
-        This is a callback for the widget's <Configure>. This method enables the widget to shrink and grow when resized
-        and if permitted by the `pack`, `grid`, or `place` geometry managers.
+        This is a callback for the widget's *<Configure>* event. This method enables the widget to shrink and grow when
+        resized and if permitted by the `pack`, `grid`, or `place` geometry managers.
 
-        :param event: the <Configure> event that is triggered when the gauge widget is resized.
+        :param event: the *<Configure>* event that is triggered when the gauge widget is resized.
         """
         padding = 4  # not sure why this is required, but if I don't include it it growths continually
         self.size = max(min(event.width - padding, event.height - padding), 100)
@@ -105,16 +119,19 @@ class RadialGauge(ttk.Label):
         """
         Redraw the arc image based on the ``variable`` value.
 
-        If invoked by a trace, the *args include: variable_name, array_index, mode. See ``tkinter.trace_add``
-        for more information on this functionality. https://docs.oracle.com/cd/E88353_01/html/E37839/trace-1t.html
+        If invoked by a trace, the \*args include: `variable_name`, `array_index`, and `mode`. See tkinter.trace_add_
+        for more information on this functionality.
 
+        .. _tkinter.trace_add: https://docs.oracle.com/cd/E88353_01/html/E37839/trace-1t.html
         """
         var = float(self.variable.get()) / self.maximum
-        angle = 450 if var >= 1.0 else (int(var * 360) + 90)
-        self.im = Image.new('RGBA', (1000, 1000))
+        endangle = (360 + self.startangle) if var >= 1.0 else (int(var * 360) + self.startangle)
+        self.im = Image.new('RGBA', (self.size * 10, self.size * 10))
         draw = ImageDraw.Draw(self.im)
-        draw.arc((0, 0, 990, 990), 0, 360, self.troughcolor, 100)
-        draw.arc((0, 0, 990, 990), 90, angle, self.indicatorcolor, 100)
+        draw.ellipse((0, 0, ((self.size * 10) - 10), ((self.size * 10) - 10)), outline=self.troughcolor,
+                     width=self.indicatorthickness * 10)
+        draw.arc((0, 0, ((self.size * 10) - 10), (self.size * 10) - 10), self.startangle, endangle, self.indicatorcolor,
+                 self.indicatorthickness * 10)
         self.arc = ImageTk.PhotoImage(self.im.resize((self.size, self.size), Image.CUBIC))
         self.configure(image=self.arc)
 
@@ -126,10 +143,10 @@ def test_radial_gauge():
     # Create the main window
     root = tk.Tk()
     root.geometry('400x400')
-    style = Style('cosmo')
+    style = Style('minty')
 
     # Create the gauge widget
-    gauge = RadialGauge(root, styler=style, font='helvetica 14 bold', maximum=360, value=275, text='275 deg')
+    gauge = RadialGauge(root, font='helvetica 14 bold', maximum=360, value=275, text='275 deg')
     gauge.pack(fill='both', expand='yes', padx=5, pady=20)
     gauge.variable.trace_add('write', lambda *args, g=gauge: g.textvariable.set(f'{g.variable.get()} deg'))
 
