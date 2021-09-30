ad25c43307b35b783aa21374dce9697ae91bf319
daniilS <daniil.soloviev@gmail.com>
2021-09-30 15:19:02 +0100

    Add support for Python 3.6 using importlib_resources

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
-"""
-Because I'm using importlib.resources, which did not exist until python 3.7, this does not support < 3.7 unless I can
-figure out another method for importing package resources.
-"""
 import setuptools
 
 long_description = """
-A collection of modern flat themes inspired by Bootstrap. There are more than a dozen built-in themes, and you also have 
+A collection of modern flat themes inspired by Bootstrap. There are more than a dozen built-in themes, and you also have
 the ability to easily create your own.
 
 ## Links
-- **Documentation:** https://ttkbootstrap.readthedocs.io/en/latest/  
+- **Documentation:** https://ttkbootstrap.readthedocs.io/en/latest/
 - **GitHub:** https://github.com/israel-dryer/ttkbootstrap
 """
 
@@ -31,6 +27,9 @@ setuptools.setup(
     packages=setuptools.find_packages(where="src"),
     package_data={"": ["*.json", "*.ttf"]},
     include_package_data=True,
-    install_requires=["pillow>=8.2.0"],
-    python_requires=">=3.7",
+    install_requires=[
+        "pillow>=8.2.0",
+        "importlib_resources; python_version < '3.7'"
+    ],
+    python_requires=">=3.6",
 )
