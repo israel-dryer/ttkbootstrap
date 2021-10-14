48322defcb97e5c95ac5ea355c0da190e84b2a6e
Israel Dryer <israel.dryer@us.gt.com>
2021-10-13 21:45:39 -0400

    add scale style test

---
+++ tests/test_scale.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from tkinter import ttk
+from ttkbootstrap import Style
+
+DARK = 'superhero'
+LIGHT = 'flatly'
+
+def create_scale_frame(widget_style, style, orient):
+    frame = ttk.Frame(root, padding=5)
+    
+    # title
+    title = ttk.Label(frame, text=widget_style, anchor=tk.CENTER)
+    title.pack(padx=5, pady=2, fill=tk.BOTH)
+    ttk.Separator(frame).pack(padx=5, pady=5, fill=tk.X)
+
+    # default
+    pb = ttk.Scale(frame, style=widget_style, orient=orient, value=0.2)
+    if orient == 'h':
+        pb.pack(padx=5, pady=5, fill=tk.BOTH)
+    else:
+        pb.pack(padx=5, pady=5, fill=tk.BOTH, side='left')
+
+    # colored
+    for i, color in enumerate(style.colors):
+        pb_style = f'{color}.{widget_style}'
+        ttk.Label(frame, text=pb_style).pack(fill=tk.X)
+        pb = ttk.Scale(frame, value=0.2, style=pb_style, orient=orient)
+        if orient == 'h':
+            pb.pack(padx=5, pady=5, fill=tk.BOTH)
+        else:
+            pb.pack(padx=5, pady=5, fill=tk.BOTH, side='left')
+
+
+    return frame
+
+if __name__ == '__main__':
+    # create visual widget style tests
+    root = tk.Tk()
+    style = Style(theme=LIGHT)
+
+    test1 = create_scale_frame('Horizontal.TScale', style, 'h')
+    test1.pack(side=tk.LEFT, anchor=tk.N)
+    test2 = create_scale_frame('Vertical.TScale', style, 'v')
+    test2.pack(side=tk.LEFT, anchor=tk.N)
+
+    root.mainloop()
