2a59d972c3284ff0fb54623440e1ad96b0144a0f
Israel Dryer <israel.dryer@us.gt.com>
2021-10-13 22:02:17 -0400

    add text style test

---
+++ tests/test_text.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+from ttkbootstrap import Style
+
+DARK = 'superhero'
+LIGHT = 'flatly'
+
+if __name__ == '__main__':
+    # create visual widget style tests
+    root = tk.Tk()
+    style = Style(theme=LIGHT)
+
+    tk.Text(root).pack(padx=10, pady=10)
+
+    root.mainloop()
