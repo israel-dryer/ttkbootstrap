a3c1b8c56aa4a3e815a0afabadf87a8cf0479c4e
Israel Dryer <israel.dryer@us.gt.com>
2021-10-13 22:15:49 -0400

    add listbox style test

---
+++ tests/test_listbox.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+from tkinter import ttk
+from ttkbootstrap import Style
+
+DARK = 'superhero'
+LIGHT = 'flatly'
+
+if __name__ == '__main__':
+    # create visual widget style tests
+    root = tk.Tk()
+    style = Style(theme=LIGHT)
+
+    lb = tk.Listbox(root)
+    lb.pack(padx=10, pady=10)
+    lb.insert('end', *list(range(5)))
+
+    root.mainloop()
