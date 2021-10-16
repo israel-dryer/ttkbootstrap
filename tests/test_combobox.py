736b9dcf7507072227062b7fc173c44013b2e413
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2021-10-15 22:07:36 -0400

    refactor combobox style

--- tests/test_combobox.py
+++ tests/test_combobox.py
@@ -39,7 +39,7 @@ def create_combobox_test(widget_style, style):
 if __name__ == '__main__':
     # create visual widget style tests
     root = tk.Tk()
-    style = Style(theme=LIGHT)
+    style = Style(theme=DARK)
 
     test1 = create_combobox_test('TCombobox', style)
     test1.pack(side=tk.LEFT, fill=tk.BOTH)
