53cbce3afc8581792659be56d087682502b85776
= <=>
2021-11-07 14:29:40 -0500

    add override for __init__ for legacy tkinter widgets

--- src/ttkbootstrap/style/bootstyle.py
+++ src/ttkbootstrap/style/bootstyle.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 from ttkbootstrap.constants import DEFAULT
 from ttkbootstrap.style.style import Style
-from ttkbootstrap.style.style_builder import StyleBuilderTTK
+from ttkbootstrap.style.style_builder import StyleBuilderTK, StyleBuilderTTK
 import ttkbootstrap.style.utility as util
 from ttkbootstrap.style.publisher import Publisher, Channel
 
@@ -29,22 +29,22 @@ TTK_WIDGETS = (
 )
 
 TK_WIDGETS = (
-    tk.Button,
-    tk.Frame,
-    tk.Label,
-    tk.Listbox,
+    # tk.Button,
+    # tk.Frame,
+    # tk.Label,
+    # tk.Listbox,
     tk.Text,
-    tk.OptionMenu
+    # tk.OptionMenu
 )
 
 
 def override_ttk_widget_constructor(func):
     """Override widget constructors with bootstyle api options"""
-    
+
     def __init__wrapper(self, *args, **kwargs):
-        
+
         ttkstyle = util.get_ttkstyle_name(self, **kwargs)
-        
+
         if ttkstyle:
             kwargs.update(style=ttkstyle)
 
@@ -53,15 +53,15 @@ def override_ttk_widget_constructor(func):
 
         # instantiate the widget
         func(self, *args, **kwargs)
-        
+
         # create style if not existing
         if ttkstyle is not None:
             update_ttk_widget_style(self, ttkstyle)
 
         # subscriber to <<ThemeChanged>> events
         Publisher.subscribe(
-            name=self._name, 
-            func=lambda widget=self: update_ttk_widget_style(widget), 
+            name=self._name,
+            func=lambda widget=self: update_ttk_widget_style(widget),
             channel=Channel.TTK
         )
 
@@ -81,8 +81,8 @@ def override_ttk_widget_configure(func):
         ttkstyle = util.get_ttkstyle_name(self, **kwargs)
         if ttkstyle:
             kwargs.update(style=ttkstyle)
-            update_ttk_widget_style(self, ttkstyle)        
-            
+            update_ttk_widget_style(self, ttkstyle)
+
         if 'bootstyle' in kwargs:
             kwargs.pop('bootstyle')
 
@@ -91,20 +91,20 @@ def override_ttk_widget_configure(func):
     return configure_wrapper
 
 
-def update_ttk_widget_style(widget: ttk.Widget, style_string: str=None):
+def update_ttk_widget_style(widget: ttk.Widget, style_string: str = None):
     """Update the ttk style or create if not existing.
-    
+
     Parameters
     ----------
     widget: ttk.Widget
         The widget instance being updated.
-    
+
     style_string : str
         The style string to evalulate. May be the `style`, `ttkstyle`
         or `bootstyle` argument depending on the context and scenario.
     """
     style: Style = Style.get_instance()
-    
+
     # get widget style if not provided
     if style_string is None:
         style_string = widget.cget('style')
@@ -125,18 +125,62 @@ def update_ttk_widget_style(widget: ttk.Widget, style_string: str=None):
 
 
 def setup_ttkbootstap_api():
+    
+    # TTK WIDGETS
     for widget in TTK_WIDGETS:
-        
         # override widget constructor
         __init = override_ttk_widget_constructor(widget.__init__)
         widget.__init__ = __init
-        
+
         # override configure method
         __configure = override_ttk_widget_configure(widget.configure)
         widget.configure = __configure
-        
+
         # override get and set methods
-        __setitem = lambda self, key, val: __configure(self, **{key: val})
-        __getitem = lambda self, key: __configure(self, cnf=key)
+        def __setitem(self, key, val): return __configure(self, **{key: val})
+        def __getitem(self, key): return __configure(self, cnf=key)
         widget.__setitem__ = __setitem
         widget.__getitem__ = __getitem
+
+    # TK WIDGETS
+    for widget in TK_WIDGETS:
+
+        # override widget constructor
+        __init = override_tk_widget_constructor(widget.__init__)
+        widget.__init__ = __init
+
+
+def override_tk_widget_constructor(func):
+    """Override widget constructors to apply default style for tk 
+    widgets
+    """
+
+    def __init__wrapper(self, *args, **kwargs):
+
+        # instantiate the widget
+        func(self, *args, **kwargs)
+        update_tk_widget_style(self)
+
+        # subscriber to <<ThemeChanged>> events
+        Publisher.subscribe(
+            name=self._name,
+            func=lambda widget=self: update_tk_widget_style(widget),
+            channel=Channel.STD
+        )
+    return __init__wrapper
+
+def update_tk_widget_style(widget: tk.Widget):
+    """Update the tk widget style
+
+    Parameters
+    ----------
+    widget: tk.Widget
+        The widget instance being updated.
+    """
+    print(widget.cget('background'))
+    style: Style = Style.get_instance()
+    widget_class = widget.__class__.__name__
+    method_name = util.tkupdate_method_name_from_string(widget_class)
+    builder: StyleBuilderTK = style.get_builder_tk()
+    builder_method = builder.name_to_method(method_name)
+    builder_method(builder, widget)
