5dc0c7a6deea0f712fc38493019e608221133f06
= <=>
2021-11-11 12:41:38 -0500

    import ttk widgets into widgets module for ttkbootstrap

--- src/ttkbootstrap/widgets/__init__.py
+++ src/ttkbootstrap/widgets/__init__.py
@@ -1,2 +1,10 @@
 from .meter import Meter
 from .floodgauge import Floodgauge
+
+from tkinter.ttk import Button, Checkbutton, Combobox
+from tkinter.ttk import Entry, Frame, Label
+from tkinter.ttk import Labelframe, LabelFrame, Menubutton
+from tkinter.ttk import Notebook, OptionMenu, PanedWindow
+from tkinter.ttk import Panedwindow, Progressbar, Radiobutton
+from tkinter.ttk import Scale, Scrollbar, Separator
+from tkinter.ttk import Sizegrip, Spinbox, Treeview
