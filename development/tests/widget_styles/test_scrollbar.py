953681fc0ba38abcb378d7f1a00a4db4947f5842
= <=>
2021-11-15 18:45:01 -0500

    update scrollbar test

--- development/tests/widget_styles/test_scrollbar.py
+++ development/tests/widget_styles/test_scrollbar.py
@@ -5,16 +5,17 @@ from random import choice
 DARK = 'superhero'
 LIGHT = 'flatly'
 
-def create_scrollbar_frame(style, orient):
+def create_scrollbar_frame(style, orient, bootstyle=''):
     frame = ttk.Frame(root, padding=5)
     
     # title
-    title = ttk.Label(frame, text='Scrollbar', anchor=tk.CENTER)
+    title = ttk.Label(frame, text=bootstyle.title() + ' Scrollbar', anchor=tk.CENTER)
     title.pack(padx=5, pady=2, fill=tk.BOTH)
     ttk.Separator(frame).pack(padx=5, pady=5, fill=tk.X)
 
     # default
-    sb = ttk.Scrollbar(frame, orient=orient)
+    ttk.Label(frame, text='default').pack(fill=tk.X)
+    sb = ttk.Scrollbar(frame, orient=orient, bootstyle=bootstyle)
     sb.set(0.1, 0.3)
     if orient == tk.HORIZONTAL:
         sb.pack(padx=5, pady=5, fill=tk.X)
@@ -22,9 +23,9 @@ def create_scrollbar_frame(style, orient):
         sb.pack(padx=5, pady=5, fill=tk.Y, side=tk.LEFT)
 
     # colored
-    for i, color in enumerate(style.colors):
+    for _, color in enumerate(style.colors):
         ttk.Label(frame, text=color).pack(fill=tk.X)
-        sb = ttk.Scrollbar(frame, bootstyle=color, orient=orient)
+        sb = ttk.Scrollbar(frame, bootstyle=(color, bootstyle), orient=orient)
         sb.set(0.1, 0.3)
         if orient == tk.HORIZONTAL:
             sb.pack(padx=5, pady=5, fill=tk.X, expand=tk.YES)
@@ -42,6 +43,7 @@ def change_style():
 if __name__ == '__main__':
     # create visual widget style tests
     root = tk.Tk()
+    root.geometry('1000x500')
     style = ttk.Style()
 
     ttk.Button(text="Change Theme", command=change_style).pack(padx=10, pady=10)
@@ -56,5 +58,20 @@ if __name__ == '__main__':
         orient=tk.VERTICAL
     )
     test2.pack(side=tk.LEFT, anchor=tk.N, fill=tk.BOTH, expand=tk.YES)
+
+    test3 = create_scrollbar_frame(
+        style=style, 
+        orient=tk.HORIZONTAL,
+        bootstyle='round'
+    )
+    test3.pack(side=tk.LEFT, anchor=tk.N, fill=tk.BOTH, expand=tk.YES)
+    
+    test4 = create_scrollbar_frame(
+        style=style, 
+        orient=tk.VERTICAL,
+        bootstyle='round'
+    )
+    test4.pack(side=tk.LEFT, anchor=tk.N, fill=tk.BOTH, expand=tk.YES)
+
 
     root.mainloop()
