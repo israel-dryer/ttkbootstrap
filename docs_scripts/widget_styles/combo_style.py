f7a636ec9d8bbb84ddfa0ca8fb50223b2d1e960e
= <=>
2021-11-22 13:41:03 -0500

    add readonly state colors for combobox

--- docs_scripts/widget_styles/combo_style.py
+++ docs_scripts/widget_styles/combo_style.py
@@ -9,17 +9,38 @@ style = ttk.Style()
 frame = ttk.Frame(padding=5)
 frame.pack(padx=5, pady=5, fill=tk.X)
 
-top_frame = ttk.Frame(frame)
-bot_frame = ttk.Frame(frame)
-top_frame.pack(fill=tk.X)
-bot_frame.pack(fill=tk.X)
+lbl1 = ttk.Label(frame, text='Normal Colors')
+frame1 = ttk.Frame(frame)
+frame2 = ttk.Frame(frame)
+
+lbl2 = ttk.Label(frame, text='State Colors')
+frame3 = ttk.Frame(frame)
+
+lbl1.pack(fill=tk.X)
+frame1.pack(fill=tk.X)
+frame2.pack(fill=tk.X, pady=(0, 10))
+lbl2.pack(fill=tk.X)
+frame3.pack(fill=tk.X)
 
 for i, color in enumerate(['default', *style.colors]):
     if i < 5:
-        a = ttk.Combobox(top_frame, bootstyle=color)
+        a = ttk.Combobox(frame1, bootstyle=color)
     else:
-        a = ttk.Combobox(bot_frame, bootstyle=color)
+        a = ttk.Combobox(frame2, bootstyle=color)
     a.insert('end', color)
     a.pack(side=tk.LEFT, padx=3, pady=10)
+
+# disabled
+a = ttk.Combobox(frame3)
+a.insert(tk.END, 'disabled')
+a.pack(side=tk.LEFT, padx=3, pady=10)
+a.configure(state=tk.DISABLED)
+
+# readonly
+a = ttk.Combobox(frame3)
+a.insert(tk.END, 'readonly')
+a.pack(side=tk.LEFT, padx=3, pady=10)
+a.configure(state='readonly')
+
 
 root.mainloop()
