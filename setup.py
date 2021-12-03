e0f37db9eb820ee33a66cf1c1b5dfc48a0a492f4
= <=>
2021-12-03 01:23:31 -0500

    update version

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ A collection of modern flat themes inspired by Bootstrap. There are more than a 
 the ability to easily create your own.
 
 ## Links
-- **Documentation:** https://ttkbootstrap.readthedocs.io/en/latest/
+- **Documentation:** https://ttkbootstrap.readthedocs.io/en/version-0.5/
 - **GitHub:** https://github.com/israel-dryer/ttkbootstrap
 """
 
 setuptools.setup(
     name="ttkbootstrap",
-    version="0.5.1",
+    version="0.5.2",
     author="Israel Dryer",
     author_email="israel.dryer@gmail.com",
     description="A collection of modern ttk themes inspired by Bootstrap",
@@ -25,8 +25,6 @@ setuptools.setup(
     url="https://github.com/israel-dryer/ttkbootstrap",
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    package_data={"": ["*.json", "*.ttf"]},
-    include_package_data=True,
     install_requires=["pillow>=8.2.0"],
     python_requires=">=3.6",
 )
