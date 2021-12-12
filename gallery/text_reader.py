69da69b8a8e03a83d0f868b5db77a2d3bb13707b
= <=>
2021-12-12 13:51:57 -0500

    refactor text reader with Window class

--- gallery/text_reader.py
+++ gallery/text_reader.py
@@ -1,67 +1,39 @@
 """
     Author: Israel Dryer
-    Modified: 2021-11-10
+    Modified: 2021-12-12
 """
-import tkinter as tk
 import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 from tkinter.filedialog import askopenfilename
 from tkinter.scrolledtext import ScrolledText
-from ttkbootstrap import utility
-utility.enable_high_dpi_awareness()
-
-class Application(tk.Tk):
-
-    def __init__(self):
-        super().__init__()
-        self.title('Text Reader')
-        self.style = ttk.Style("sandstone")
-        self.reader = Reader(self)
-        self.reader.pack(fill=tk.BOTH, expand=tk.YES)
 
 
-class Reader(ttk.Frame):
+class TextReader(ttk.Frame):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.configure(padding=10)
-        self.filename = tk.StringVar()
+    def __init__(self, master):
+        super().__init__(master, padding=15)
+        self.filename = ttk.StringVar()
+        self.pack(fill=BOTH, expand=YES)
+        self.create_widget_elements()
 
-        # scrolled text with custom highlight colors
-        self.text_area = ScrolledText(
+    def create_widget_elements(self):
+        """Create and add the widget elements"""
+        style = ttk.Style()
+        self.textbox = ScrolledText(
             master=self,
-            highlightcolor=self.master.style.colors.primary,
-            highlightbackground=self.master.style.colors.border,
+            highlightcolor=style.colors.primary,
+            highlightbackground=style.colors.border,
             highlightthickness=1
         )
-        self.text_area.pack(fill=tk.BOTH)
+        self.textbox.pack(fill=BOTH)
+        default_txt = "Click the browse button to open a new text file."
+        self.textbox.insert(END, default_txt)
 
-        # insert default text in text area
-        self.text_area.insert(
-            tk.END, 'Click the browse button to open a new text file.')
+        file_entry = ttk.Entry(self, textvariable=self.filename)
+        file_entry.pack(side=LEFT, fill=X, expand=YES, padx=(0, 5), pady=10)
 
-        # filepath
-        ttk.Entry(
-            master=self,
-            textvariable=self.filename
-        ).pack(
-            side=tk.LEFT,
-            fill=tk.X,
-            expand=tk.YES,
-            padx=(0, 5),
-            pady=10
-        )
-
-        # browse button
-        ttk.Button(
-            master=self,
-            text='Browse',
-            command=self.open_file
-        ).pack(
-            side=tk.RIGHT,
-            fill=tk.X,
-            padx=(5, 0),
-            pady=10
-        )
+        browse_btn = ttk.Button(self, text="Browse", command=self.open_file)
+        browse_btn.pack(side=RIGHT, fill=X, padx=(5, 0), pady=10)
 
     def open_file(self):
         path = askopenfilename()
@@ -69,10 +41,13 @@ class Reader(ttk.Frame):
             return
 
         with open(path, encoding='utf-8') as f:
-            self.text_area.delete('1.0', tk.END)
-            self.text_area.insert(tk.END, f.read())
+            self.textbox.delete('1.0', END)
+            self.textbox.insert(END, f.read())
             self.filename.set(path)
 
 
 if __name__ == '__main__':
-    Application().mainloop()
+    
+    app = ttk.Window("Text Reader", "sandstone")
+    TextReader(app)
+    app.mainloop()
