7a2268d71c70e2a9b8ba21b3b0b723148bd67124
= <=>
2021-12-12 15:18:24 -0500

    add Toplevel class, add attributes to Window class

--- src/ttkbootstrap/__init__.py
+++ src/ttkbootstrap/__init__.py
@@ -1,7 +1,7 @@
 from ttkbootstrap.style import Style
 from ttkbootstrap.style import Bootstyle
 from ttkbootstrap.widgets import *
-from ttkbootstrap.window import Window
+from ttkbootstrap.window import Window, Toplevel
 from tkinter import Variable, StringVar, IntVar, BooleanVar, DoubleVar
 from tkinter import PhotoImage
 Bootstyle.setup_ttkbootstap_api()
