70f8b47160d5747bd44f4224f6c38537862b318b
= <=>
2021-12-12 13:41:06 -0500

    refactor long-running task with Window class

--- gallery/long_running_determinate.py
+++ gallery/long_running_determinate.py
@@ -1,88 +1,88 @@
+import textwrap
+from time import sleep
 from queue import Queue
 from random import randint
 from threading import Thread
-from time import sleep
-import tkinter as tk
 import ttkbootstrap as ttk
-from tkinter.messagebox import showinfo
-from ttkbootstrap import utility
-utility.enable_high_dpi_awareness()
-
-
-class Application(tk.Tk):
-
-    def __init__(self):
-        super().__init__()
-        self.title('Long Running Operation - Determinate')
-        self.style = ttk.Style('lumen')
-
-        # set the main background color to primary, then add 10px 
-        # padding to create a thick border effect
-        self.configure(background=self.style.colors.primary)
-        self.lr = LongRunning(self)
-        self.lr.pack(fill=tk.BOTH, expand=tk.YES, padx=10, pady=10)
+from ttkbootstrap.constants import *
+from ttkbootstrap.dialogs import Messagebox
 
 
 class LongRunning(ttk.Frame):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.configure(padding=20)
-        self.task_queue = Queue()
+    threadqueue = Queue()
 
-        # instructions
-        _text = '\n'.join([
-            "Click the START button to begin a",
-            "long-running task that will last",
-            "approximately 1 to 15 seconds"
-        ])
-        lbl = ttk.Label(self, text=_text, justify=tk.LEFT)
-        lbl.pack(fill=tk.X, pady=10)
+    def __init__(self, master):
+        super().__init__(master, padding=5, bootstyle=INFO)
+        self.pack(fill=BOTH, expand=YES)
+        self.message = ttk.StringVar(value='')
+        self.create_elements()
 
-        # start button
-        self.btn = ttk.Button(self, text="START", command=self.start_task)
-        self.btn.pack(pady=10)
+    def create_elements(self):
+        """Create the layout elements."""
+        container = ttk.Frame(self, padding=10)
+        container.pack(fill=BOTH, expand=YES)
+        
+        _text = ("Click the START button to begin a long-running task " + 
+                 "that will last approximately 1 to 15 seconds.")
+        wrapped = '\n'.join(textwrap.wrap(_text, width=35))
+        lbl = ttk.Label(container, text=wrapped, justify=LEFT)
+        lbl.pack(fill=X, pady=10, expand=YES)
 
-        # determinate progress bar (for know number of steps)
-        self.progressbar = ttk.Progressbar(
-            master=self, 
-            maximum=10, 
-            bootstyle='info-striped'
+        self.start_btn = ttk.Button(
+            master=container, 
+            text='START',
+            command=self.start_task
         )
-        self.progressbar.pack(fill=tk.X)
-
-    def simulated_blocking_io_task(self, thread_num):
-        """A simulated IO operation to run for a random time interval 
-        between 1 and 15 seconds"""
-        seconds_to_run = randint(1, 15)
-        sleep(seconds_to_run)
-        self.task_queue.task_done()
-        print('Finished task on Thread:', thread_num)
+        self.start_btn.pack(fill=X, pady=10)
+        self.progressbar = ttk.Progressbar(
+            master=container, 
+            maximum=10, 
+            bootstyle=(STRIPED, SUCCESS)
+        )
+        self.progressbar.pack(fill=X, expand=YES)
+        msg_lbl = ttk.Label(container, textvariable=self.message, anchor=CENTER)
+        msg_lbl.pack(fill=X, pady=10)
 
     def start_task(self):
         """Start the progressbar and run the task in another thread"""
-        self.btn.configure(state=tk.DISABLED)
+        self.start_btn.configure(state=DISABLED)
         for i in range(1, 11):
-            self.task_queue.put(
-                Thread(
-                    target=self.simulated_blocking_io_task, 
-                    args=[i], daemon=True
-                ).start())
+            thread = Thread(
+                target=self.simulate_io_task, 
+                args=[i], 
+                daemon=True
+            )
+            LongRunning.threadqueue.put(thread.start())
         self.listen_for_complete_task()
 
     def listen_for_complete_task(self):
         """Check to see if task is complete; if so, stop the 
         progressbar and show and alert
         """
-        if self.task_queue.unfinished_tasks == 0:
+        if LongRunning.threadqueue.unfinished_tasks == 0:
             self.progressbar.configure(value=10)
-            showinfo(title='alert', message="process complete")
-            self.btn.configure(state=tk.NORMAL)
+            Messagebox.ok(title='alert', message="process complete")
+            self.start_btn.configure(state=NORMAL)
+            self.message.set('')
             return
-        tasks_completed = self.progressbar.cget('maximum') - self.task_queue.unfinished_tasks
+        max_value = self.progressbar.cget('maximum')
+        tasks_completed = max_value - LongRunning.threadqueue.unfinished_tasks
         self.progressbar.configure(value=tasks_completed)
         self.after(500, self.listen_for_complete_task)
 
+    def simulate_io_task(self, threadnum):
+        """Simulate an IO operation to run for a random interval 
+        between 1 and 15 seconds.
+        """
+        seconds_to_run = randint(1, 15)
+        sleep(seconds_to_run)
+        LongRunning.threadqueue.task_done()
+        self.message.set(f'Finished task on Thread: {threadnum}')
+
 
 if __name__ == '__main__':
-    Application().mainloop()
+
+    app = ttk.Window(title="task", themename="lumen")
+    LongRunning(app)
+    app.mainloop()
