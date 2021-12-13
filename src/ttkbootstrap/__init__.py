abd787f2662352c8d1bc08b3b30156e069d40c63
= <=>
2021-12-12 22:55:21 -0500

    add Canvas, Menu, and Text to imports

--- src/ttkbootstrap/__init__.py
+++ src/ttkbootstrap/__init__.py
@@ -4,5 +4,5 @@ from ttkbootstrap.widgets import *
 from ttkbootstrap.window import Window, Toplevel
 from tkinter import Variable, StringVar, IntVar, BooleanVar, DoubleVar
 from tkinter import PhotoImage
-from tkinter import Canvas
+from tkinter import Canvas, Menu, Text
 Bootstyle.setup_ttkbootstap_api()
