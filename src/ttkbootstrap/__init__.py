3b8b544515d9678090d380719f4b57bfbfa76c42
= <=>
2021-12-12 22:33:50 -0500

    add Canvas to imports

--- src/ttkbootstrap/__init__.py
+++ src/ttkbootstrap/__init__.py
@@ -4,4 +4,5 @@ from ttkbootstrap.widgets import *
 from ttkbootstrap.window import Window, Toplevel
 from tkinter import Variable, StringVar, IntVar, BooleanVar, DoubleVar
 from tkinter import PhotoImage
+from tkinter import Canvas
 Bootstyle.setup_ttkbootstap_api()
