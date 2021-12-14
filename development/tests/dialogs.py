13772ce9293c9a94ab314b4911d77d6228bc628a
Israel Dryer <israel.dryer@gmail.com>
2021-12-14 00:16:11 -0500

    update dialogs test

--- development/tests/dialogs.py
+++ development/tests/dialogs.py
@@ -1,8 +1,5 @@
-import tkinter as tk
-import ttkbootstrap as ttk
 from ttkbootstrap.dialogs import Querybox, Messagebox
 
-root = tk.Tk()
 Messagebox.ok("Do you want to continue?")
 Messagebox.retrycancel("Should I retry?")
 Messagebox.okcancel("A message here")
