afb3508022d5e9953ddd40de2f6cee2cea0fbea4
Israel Dryer <israel.dryer@gmail.com>
2021-12-14 00:17:21 -0500

    add imports

--- src/ttkbootstrap/__init__.py
+++ src/ttkbootstrap/__init__.py
@@ -2,7 +2,10 @@ from ttkbootstrap.style import Style
 from ttkbootstrap.style import Bootstyle
 from ttkbootstrap.widgets import *
 from ttkbootstrap.window import Window, Toplevel
+
+from tkinter.scrolledtext import ScrolledText
 from tkinter import Variable, StringVar, IntVar, BooleanVar, DoubleVar
-from tkinter import PhotoImage
 from tkinter import Canvas, Menu, Text
+from tkinter import PhotoImage
+
 Bootstyle.setup_ttkbootstap_api()
