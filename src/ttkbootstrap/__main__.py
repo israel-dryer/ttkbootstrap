e12a6355034271b7f719e53a005105d281e33c15
Israel Dryer <israel.dryer@gmail.com>
2021-12-14 22:30:47 -0500

    added Messagebox.ok to outline button; black autoformatting.

--- src/ttkbootstrap/__main__.py
+++ src/ttkbootstrap/__main__.py
@@ -6,6 +6,7 @@
 """
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
+from ttkbootstrap.dialogs import Messagebox
 
 
 def setup_demo(master):
@@ -38,9 +39,7 @@ Namespaces are one honking great idea -- let's do more of those!"""
     theme_selection.pack(fill=X, expand=YES)
 
     theme_selected = ttk.Label(
-        master=theme_selection, 
-        text="litera", 
-        font="-size 24 -weight bold"
+        master=theme_selection, text="litera", font="-size 24 -weight bold"
     )
     theme_selected.pack(side=LEFT)
 
@@ -60,10 +59,10 @@ Namespaces are one honking great idea -- let's do more of those!"""
         t = cbo.get()
         style.theme_use(t)
         theme_selected.configure(text=t)
-        theme_cbo.selection_clear()    
+        theme_cbo.selection_clear()
         default.focus_set()
 
-    theme_cbo.bind('<<ComboboxSelected>>', change_theme)
+    theme_cbo.bind("<<ComboboxSelected>>", change_theme)
 
     lframe = ttk.Frame(root, padding=5)
     lframe.pack(side=LEFT, fill=BOTH, expand=YES)
@@ -72,9 +71,7 @@ Namespaces are one honking great idea -- let's do more of those!"""
     rframe.pack(side=RIGHT, fill=BOTH, expand=YES)
 
     color_group = ttk.Labelframe(
-        master=lframe,
-        text="Theme color options",
-        padding=10
+        master=lframe, text="Theme color options", padding=10
     )
     color_group.pack(fill=X, side=TOP)
 
@@ -83,31 +80,29 @@ Namespaces are one honking great idea -- let's do more of those!"""
         cb.pack(side=LEFT, expand=YES, padx=5, fill=X)
 
     rb_group = ttk.Labelframe(
-        lframe, text="Checkbuttons & radiobuttons", padding=10)
+        lframe, text="Checkbuttons & radiobuttons", padding=10
+    )
     rb_group.pack(fill=X, pady=10, side=TOP)
 
     check1 = ttk.Checkbutton(rb_group, text="selected")
     check1.pack(side=LEFT, expand=YES, padx=5)
     check1.invoke()
-    
+
     check2 = ttk.Checkbutton(rb_group, text="deselected")
     check2.pack(side=LEFT, expand=YES, padx=5)
-    
+
     check3 = ttk.Checkbutton(rb_group, text="disabled", state=DISABLED)
     check3.pack(side=LEFT, expand=YES, padx=5)
 
     radio1 = ttk.Radiobutton(rb_group, text="selected", value=1)
     radio1.pack(side=LEFT, expand=YES, padx=5)
     radio1.invoke()
-    
+
     radio2 = ttk.Radiobutton(rb_group, text="deselected", value=2)
     radio2.pack(side=LEFT, expand=YES, padx=5)
-    
+
     radio3 = ttk.Radiobutton(
-        master=rb_group, 
-        text="disabled",
-        value=3, 
-        state=DISABLED
+        master=rb_group, text="disabled", value=3, state=DISABLED
     )
     radio3.pack(side=LEFT, expand=YES, padx=5)
 
@@ -115,75 +110,44 @@ Namespaces are one honking great idea -- let's do more of those!"""
     ttframe.pack(pady=5, fill=X, side=TOP)
 
     table_data = [
-        ('South Island, New Zealand', 1),
-        ('Paris', 2),
-        ('Bora Bora', 3),
-        ('Maui', 4),
-        ('Tahiti', 5)
+        ("South Island, New Zealand", 1),
+        ("Paris", 2),
+        ("Bora Bora", 3),
+        ("Maui", 4),
+        ("Tahiti", 5),
     ]
 
-    tv = ttk.Treeview(
-        master=ttframe,
-        columns=[0, 1],
-        show=HEADINGS,
-        height=5
-    )
+    tv = ttk.Treeview(master=ttframe, columns=[0, 1], show=HEADINGS, height=5)
     for row in table_data:
-        tv.insert('', END, values=row)
+        tv.insert("", END, values=row)
 
-    tv.selection_set('I001')
-    tv.heading(0, text='City')
-    tv.heading(1, text='Rank')
+    tv.selection_set("I001")
+    tv.heading(0, text="City")
+    tv.heading(1, text="Rank")
     tv.column(0, width=300)
     tv.column(1, width=70, anchor=CENTER)
     tv.pack(side=LEFT, anchor=NE, fill=X)
 
     # # notebook with table and text tabs
     nb = ttk.Notebook(ttframe)
-    nb.pack(
-        side=LEFT,
-        padx=(10, 0),
-        expand=YES,
-        fill=BOTH
-    )
+    nb.pack(side=LEFT, padx=(10, 0), expand=YES, fill=BOTH)
     nb_text = "This is a notebook tab.\nYou can put any widget you want here."
     nb.add(ttk.Label(nb, text=nb_text), text="Tab 1", sticky=NW)
     nb.add(
-        child=ttk.Label(nb, text="A notebook tab."),
-        text="Tab 2",
-        sticky=NW
+        child=ttk.Label(nb, text="A notebook tab."), text="Tab 2", sticky=NW
     )
-    nb.add(ttk.Frame(nb), text='Tab 3')
-    nb.add(ttk.Frame(nb), text='Tab 4')
-    nb.add(ttk.Frame(nb), text='Tab 5')
+    nb.add(ttk.Frame(nb), text="Tab 3")
+    nb.add(ttk.Frame(nb), text="Tab 4")
+    nb.add(ttk.Frame(nb), text="Tab 5")
 
     # text widget
-    txt = ttk.Text(
-        master=lframe,
-        height=5,
-        width=50,
-        wrap='none'
-    )
+    txt = ttk.Text(master=lframe, height=5, width=50, wrap="none")
     txt.insert(END, ZEN)
-    txt.pack(
-        side=LEFT,
-        anchor=NW,
-        pady=5,
-        fill=BOTH,
-        expand=YES
-    )
+    txt.pack(side=LEFT, anchor=NW, pady=5, fill=BOTH, expand=YES)
     lframe_inner = ttk.Frame(lframe)
-    lframe_inner.pack(
-        fill=BOTH,
-        expand=YES,
-        padx=10
-    )
+    lframe_inner.pack(fill=BOTH, expand=YES, padx=10)
     s1 = ttk.Scale(
-        master=lframe_inner,
-        orient=HORIZONTAL,
-        value=75,
-        from_=100,
-        to=0
+        master=lframe_inner, orient=HORIZONTAL, value=75, from_=100, to=0
     )
     s1.pack(fill=X, pady=5, expand=YES)
 
@@ -197,16 +161,16 @@ Namespaces are one honking great idea -- let's do more of those!"""
         master=lframe_inner,
         orient=HORIZONTAL,
         value=75,
-        bootstyle=(SUCCESS, STRIPED)
+        bootstyle=(SUCCESS, STRIPED),
     ).pack(fill=X, pady=5, expand=YES)
 
     m = ttk.Meter(
         master=lframe_inner,
         metersize=150,
         amountused=45,
-        subtext='meter widget',
+        subtext="meter widget",
         bootstyle=INFO,
-        interactive=True
+        interactive=True,
     )
     m.pack(pady=10)
 
@@ -218,36 +182,27 @@ Namespaces are one honking great idea -- let's do more of those!"""
     sb.pack(fill=X, pady=5, expand=YES)
 
     sb = ttk.Scrollbar(
-        master=lframe_inner,
-        orient=HORIZONTAL,
-        bootstyle=(DANGER, ROUND)
+        master=lframe_inner, orient=HORIZONTAL, bootstyle=(DANGER, ROUND)
     )
     sb.set(0.1, 0.9)
     sb.pack(fill=X, pady=5, expand=YES)
 
-    btn_group = ttk.Labelframe(
-        master=rframe,
-        text="Buttons",
-        padding=(10, 5)
-    )
+    btn_group = ttk.Labelframe(master=rframe, text="Buttons", padding=(10, 5))
     btn_group.pack(fill=X)
 
     menu = ttk.Menu(root)
     for i, t in enumerate(style.theme_names()):
         menu.add_radiobutton(label=t, value=i)
 
-    default = ttk.Button(
-        master=btn_group,
-        text="solid button"
-    )
+    default = ttk.Button(master=btn_group, text="solid button")
     default.pack(fill=X, pady=5)
     default.focus_set()
 
     mb = ttk.Menubutton(
         master=btn_group,
         text="solid menubutton",
-        bootstyle=SECONDARY, 
-        menu=menu
+        bootstyle=SECONDARY,
+        menu=menu,
     )
     mb.pack(fill=X, pady=5)
 
@@ -262,7 +217,8 @@ Namespaces are one honking great idea -- let's do more of those!"""
     ob = ttk.Button(
         master=btn_group,
         text="outline button",
-        bootstyle=(INFO, OUTLINE)
+        bootstyle=(INFO, OUTLINE),
+        command=lambda: Messagebox.ok("You pushed an outline button"),
     )
     ob.pack(fill=X, pady=5)
 
@@ -270,22 +226,18 @@ Namespaces are one honking great idea -- let's do more of those!"""
         master=btn_group,
         text="outline menubutton",
         bootstyle=(WARNING, OUTLINE),
-        menu=menu
+        menu=menu,
     )
     mb.pack(fill=X, pady=5)
 
     cb = ttk.Checkbutton(
         master=btn_group,
         text="outline toolbutton",
-        bootstyle=(SUCCESS, OUTLINE, TOOLBUTTON)
+        bootstyle=(SUCCESS, OUTLINE, TOOLBUTTON),
     )
     cb.pack(fill=X, pady=5)
 
-    lb = ttk.Button(
-        master=btn_group,
-        text="link button",
-        bootstyle=LINK
-    )
+    lb = ttk.Button(master=btn_group, text="link button", bootstyle=LINK)
     lb.pack(fill=X, pady=5)
 
     cb1 = ttk.Checkbutton(
@@ -297,39 +249,24 @@ Namespaces are one honking great idea -- let's do more of those!"""
     cb1.pack(fill=X, pady=5)
 
     cb2 = ttk.Checkbutton(
-        master=btn_group,
-        text="squared toggle",
-        bootstyle=(SQUARE, TOGGLE)
+        master=btn_group, text="squared toggle", bootstyle=(SQUARE, TOGGLE)
     )
     cb2.pack(fill=X, pady=5)
     cb2.invoke()
 
     input_group = ttk.Labelframe(
-        master=rframe,
-        text="Other input widgets",
-        padding=10
+        master=rframe, text="Other input widgets", padding=10
     )
-    input_group.pack(
-        fill=BOTH,
-        pady=(10, 5),
-        expand=YES
-    )
+    input_group.pack(fill=BOTH, pady=(10, 5), expand=YES)
     entry = ttk.Entry(input_group)
     entry.pack(fill=X)
     entry.insert(END, "entry widget")
 
-    password = ttk.Entry(
-        master=input_group,
-        show="•"
-    )
+    password = ttk.Entry(master=input_group, show="•")
     password.pack(fill=X, pady=5)
     password.insert(END, "password")
 
-    spinbox = ttk.Spinbox(
-        master=input_group,
-        from_=0,
-        to=100
-    )
+    spinbox = ttk.Spinbox(master=input_group, from_=0, to=100)
     spinbox.pack(fill=X)
     spinbox.set(45)
 
@@ -337,7 +274,7 @@ Namespaces are one honking great idea -- let's do more of those!"""
         master=input_group,
         text=style.theme.name,
         values=theme_names,
-        exportselection=False
+        exportselection=False,
     )
     cbo.pack(fill=X, pady=5)
     cbo.current(theme_names.index(style.theme.name))
@@ -347,10 +284,11 @@ Namespaces are one honking great idea -- let's do more of those!"""
 
     return root
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     app = ttk.Window("ttkbootstrap widget demo")
-    
+
     bagel = setup_demo(app)
     bagel.pack(fill=BOTH, expand=YES)
 
