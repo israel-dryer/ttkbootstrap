36103fb653d8b7648ed920b7c92b2850d592e0ee
Israel Dryer <israel.dryer@gmail.com>
2021-12-17 13:57:37 -0500

    add show_(info, warning, question, error) convenience methods to Messagbox class; fix bug in container reference.

--- development/tests/dialogs.py
+++ development/tests/dialogs.py
@@ -1,7 +1,12 @@
 from ttkbootstrap.dialogs import Querybox, Messagebox
+from ttkbootstrap.icons import Icon
 
-Messagebox.ok("Do you want to continue?")
+Messagebox.ok("Do you want to continue?", icon=Icon.question)
 Messagebox.retrycancel("Should I retry?")
 Messagebox.okcancel("A message here")
+Messagebox.show_error("You did this wrong!")
+Messagebox.show_info("This is an info message")
+Messagebox.show_warning("This is a warning message")
+Messagebox.show_question("Do you want to continue?")
 Querybox.get_date()
 Querybox.get_font()
