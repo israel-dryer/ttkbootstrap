0f128ce21d3247ba3b2f2acd8db2f26e984c85cf
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2021-12-21 14:12:06 -0500

    change name of tooltip class

--- development/tests/tooltip.py
+++ development/tests/tooltip.py
@@ -1,19 +1,19 @@
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
-from ttkbootstrap.tooltip import Tooltip
+from ttkbootstrap.tooltip import ToolTip
 
 app = ttk.Window(size=(400, 100))
 
 b1 = ttk.Button(app, text="default tooltip")
 b1.pack(side=LEFT, padx=10, pady=10, fill=X, expand=YES)
-Tooltip(
+ToolTip(
     b1,
     text="This is the default tooltip. This yellow background and black foreground will be applied by default.",
 )
 
 b2 = ttk.Button(app, text="styled tooltip")
 b2.pack(side=LEFT, padx=10, pady=10, fill=X, expand=YES)
-Tooltip(
+ToolTip(
     b2,
     text="This is a styled tooltip. You can change this style by using the `bootstyle` parameter with label style keywords.",
     bootstyle="danger-inverse",
