9ae18ce1c932c0dcd0dca56e4647387def9bdfff
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2022-01-09 14:42:28 -0500

    update disabled color

--- docs_scripts/widget_styles/menubutton_style.py
+++ docs_scripts/widget_styles/menubutton_style.py
@@ -26,23 +26,23 @@ a = ttk.Menubutton(bot_frame, text='disabled', width=12, state=tk.DISABLED)
 a.pack(side=tk.LEFT, padx=3, pady=10)    
 
 
-# # outline menubutton
-# frame = ttk.Frame(padding=5)
-# frame.pack(padx=5, pady=5, fill=tk.X)
+# outline menubutton
+frame = ttk.Frame(padding=5)
+frame.pack(padx=5, pady=5, fill=tk.X)
 
-# top_frame = ttk.Frame(frame)
-# bot_frame = ttk.Frame(frame)
-# top_frame.pack(fill=tk.X)
-# bot_frame.pack(fill=tk.X)
+top_frame = ttk.Frame(frame)
+bot_frame = ttk.Frame(frame)
+top_frame.pack(fill=tk.X)
+bot_frame.pack(fill=tk.X)
 
-# for i, color in enumerate(['default', *style.colors]):
-#     if i < 5:
-#         a = ttk.Menubutton(top_frame, text=color, bootstyle=color + "outline", width=12)
-#     else:
-#         a = ttk.Menubutton(bot_frame, text=color, bootstyle=color + "outline", width=12)
+for i, color in enumerate(['default', *style.colors]):
+    if i < 5:
+        a = ttk.Menubutton(top_frame, text=color, bootstyle=color + "outline", width=12)
+    else:
+        a = ttk.Menubutton(bot_frame, text=color, bootstyle=color + "outline", width=12)
     
-#     a.pack(side=tk.LEFT, padx=3, pady=10)
-# a = ttk.Menubutton(bot_frame, text='disabled', width=12, bootstyle='outline', state=tk.DISABLED)
-# a.pack(side=tk.LEFT, padx=3, pady=10)    
+    a.pack(side=tk.LEFT, padx=3, pady=10)
+a = ttk.Menubutton(bot_frame, text='disabled', width=12, bootstyle='outline', state=tk.DISABLED)
+a.pack(side=tk.LEFT, padx=3, pady=10)    
 
 root.mainloop()
