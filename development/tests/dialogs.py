784d60e0cbaeab10420b2ed3e684a71d102c7e1d
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2022-01-22 23:21:31 -0500

    update dialog tests

--- development/tests/dialogs.py
+++ development/tests/dialogs.py
@@ -1,21 +1,24 @@
-from tkinter import Message
+import ttkbootstrap as ttk
 from ttkbootstrap.dialogs import Querybox, Messagebox
 from ttkbootstrap.icons import Icon
+import tkinter as tk
 
-# Messagebox.ok("Messagebox.ok")
-# Messagebox.okcancel("Messagebox.okcancel")
-# Messagebox.retrycancel("Messagebox.retrycancel")
-# Messagebox.yesno("Messagebox.yesno")
-# Messagebox.yesnocancel("Messagebox.yesnocancel")
+from ttkbootstrap.localization import MessageCatalog
+MessageCatalog.locale('zh_cn')
+Messagebox.ok("Messagebox.ok")
 
-# Messagebox.show_error("Messagebox.show_error")
-# Messagebox.show_info("Messagebox.show_info")
-result = Messagebox.show_question("Messagebox.show_question")
-print(result)
-# Messagebox.show_warning("Messagebox.show_warning")
+Messagebox.okcancel("Messagebox.okcancel")
+Messagebox.retrycancel("Messagebox.retrycancel")
+Messagebox.yesno("Messagebox.yesno")
+Messagebox.yesnocancel("Messagebox.yesnocancel")
 
-# Querybox.get_date(title="Querybox.get_date")
-# Querybox.get_float("Querybox.get_float")
-# Querybox.get_integer("Querybox.get_integer")
-# Querybox.get_string("Querybox.get_string")
-# Querybox.get_font(title="Querybox.get_font")
+Messagebox.show_error("Messagebox.show_error")
+Messagebox.show_info("Messagebox.show_info")
+Messagebox.show_question("Messagebox.show_question")
+Messagebox.show_warning("Messagebox.show_warning")
+
+Querybox.get_date(title="Querybox.get_date")
+Querybox.get_float("Querybox.get_float")
+Querybox.get_integer("Querybox.get_integer")
+Querybox.get_string("Querybox.get_string")
+Querybox.get_font(title="Querybox.get_font")
