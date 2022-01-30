00cc7c9bab510ed77c0f3f8200c33a58cb069bb1
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2022-01-30 12:35:26 -0500

    add development folder

---
+++ development/filedialogs/filebrowser.py
@@ -0,0 +1,18 @@
+
+
+class FileBrowser:
+
+    def __init__(self, parent, initialdir="", initialfile="", mode="openfile", 
+                 multipleselection=False, message="", title="Open File", 
+                 filetypes=[], okbuttontext="Ok", cancelbuttontext="Cancel", 
+                 createfolders=True
+                 ):
+        ...
+
+    
+class Stats:
+
+    ...
+
+
+
