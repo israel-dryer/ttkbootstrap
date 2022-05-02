7d2d803d550d92d16d09ebbf37dcef7582341a9e
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2022-05-01 20:23:49 -0400

    Enable font changes in Text widget (#283)
    * Set initial default text font with option database; remove default font in widget update.
    
    * Add test for font parameter.
    
    * Bump version to 1.7.6.2 (unpublished)

--- tests/widget_styles/test_text.py
+++ tests/widget_styles/test_text.py
@@ -1,4 +1,3 @@
-import tkinter as tk
 import ttkbootstrap as ttk
 from random import choice
 from ttkbootstrap import utility
@@ -7,18 +6,18 @@ utility.enable_high_dpi_awareness()
 DARK = 'superhero'
 LIGHT = 'flatly'
 
-def change_style():
-    theme = choice(style.theme_names())
-    style.theme_use(theme)    
+def change_style(window):
+    theme = choice(window.style.theme_names())
+    window.style.theme_use(theme)
 
 
 if __name__ == '__main__':
     # create visual widget style tests
-    root = tk.Tk()
-    style = ttk.Style()
+    window = ttk.Window(themename='darkly')
 
-    ttk.Button(text="Change Theme", command=change_style).pack(padx=10, pady=10)
+    ttk.Button(text="Change Theme", command=lambda x=window: change_style(x)).pack(padx=10, pady=10)
+    text = ttk.Text(window, font='helvetica 24 bold')
+    text.pack(padx=10, pady=10)
+    text.insert('end', 'Hello, this is my text.')
 
-    tk.Text(root).pack(padx=10, pady=10)
-
-    root.mainloop()
+    window.mainloop()
