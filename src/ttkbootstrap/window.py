e625892953b8e39718297443730066391220df20
laggykiller <chaudominic2@gmail.com>
2023-02-09 23:10:12 +0800

    Move iconify to before setting alpha (#236)

--- src/ttkbootstrap/window.py
+++ src/ttkbootstrap/window.py
@@ -263,16 +263,16 @@ class Window(tkinter.Tk):
             width, height = resizable
             self.resizable(width, height)
         
+        if alpha is not None:
+            if self.winsys == 'x11':
+                self.wait_visibility(self)
+            self.attributes("-alpha", alpha)
+        
         if transient is not None:
             self.transient(transient)
         
         if overrideredirect:
             self.overrideredirect(1)
-        
-        if alpha is not None:
-            if self.winsys == 'x11':
-                self.wait_visibility(self)
-            self.attributes("-alpha", alpha)
 
         apply_class_bindings(self)
         apply_all_bindings(self)
@@ -414,9 +414,6 @@ class Toplevel(tkinter.Toplevel):
 
         super().__init__(**kwargs)
         self.winsys = self.tk.call('tk', 'windowingsystem')
-        
-        if iconify:
-            self.iconify()
 
         if iconphoto != '':
             try:
@@ -450,6 +447,14 @@ class Toplevel(tkinter.Toplevel):
             width, height = resizable
             self.resizable(width, height)
         
+        if alpha is not None:
+            if self.winsys == 'x11':
+                self.wait_visibility(self)
+            self.attributes("-alpha", alpha)
+        
+        if iconify:
+            self.iconify()
+        
         if transient is not None:
             self.transient(transient)
         
@@ -466,11 +471,6 @@ class Toplevel(tkinter.Toplevel):
         if toolwindow:
             if self.winsys == 'win32':
                 self.attributes("-toolwindow", 1)
-        
-        if alpha is not None:
-            if self.winsys == 'x11':
-                self.wait_visibility(self)
-            self.attributes("-alpha", alpha)
 
     @property
     def style(self):
