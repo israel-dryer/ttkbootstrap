62411ce7d409128039f295a3f4b315a0ac6e691d
Israel Dryer <israel.dryer@gmail.com>
2025-03-30 22:06:02 -0400

    resolve conflict in existing pull request

--- src/ttkbootstrap/widgets.py
+++ src/ttkbootstrap/widgets.py
@@ -761,7 +761,6 @@ class Meter(ttk.Frame):
             text=self._subtext,
             bootstyle=(self._subtextstyle, "metersubtxt"),
             font=self._subtextfont,
-            textvariable=self.labelvar
         )
 
         self.bind("<<ThemeChanged>>", self._on_theme_change)
