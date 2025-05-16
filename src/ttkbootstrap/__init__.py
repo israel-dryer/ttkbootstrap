5af558fd0ecafd5a5995265c3d08459a901831d6
Israel Dryer <israel.dryer@gmail.com>
2025-05-16 15:55:28 -0400

    remove imports

--- src/ttkbootstrap/__init__.py
+++ src/ttkbootstrap/__init__.py
@@ -3,9 +3,4 @@ from ttkbootstrap.style import Bootstyle
 from ttkbootstrap.widgets import *
 from ttkbootstrap.window import Window, Toplevel
 
-from tkinter.scrolledtext import ScrolledText
-from tkinter import Variable, StringVar, IntVar, BooleanVar, DoubleVar
-from tkinter import Canvas, Menu, Text
-from tkinter import PhotoImage
-
 Bootstyle.setup_ttkbootstrap_api()
