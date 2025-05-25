2f832d9dff60f129867369fecef1140cd271b694
Israel Dryer <israel.dryer@gmail.com>
2025-05-25 09:33:16 -0400

    remove existing documentation and tests

--- docs/en/api/widgets/meter.md
+++
@@ -1,8 +0,0 @@
-# Meter
-
-::: ttkbootstrap.widgets.Meter
-    selection:
-        filters: ["!^_", "^__init__"]
-    rendering:
-        heading_level: 2
-        show_root_heading: true
---
+++ tests/visual/test_icons.py
@@ -0,0 +1,25 @@
+from ttkbootstrap.window import Window
+from ttkbootstrap.icons import Icon
+from ttkbootstrap.widgets import Button, CheckbuttonToggle, IconButton
+from tkinter.ttk import Button as ttkButton
+from tkinter import Label
+
+# Use in your GUI
+root = Window()
+
+IconButton(root, icon="house").pack(padx=10, pady=10)
+IconButton(root, icon="envelope-paper-heart-fill", color="secondary", variant="text").pack(padx=10, pady=10)
+Button(root, icon="apple", text="Apple", color="danger", variant="text").pack(padx=10, pady=10)
+IconButton(root, icon="apple", color="danger").pack(padx=10, pady=10)
+IconButton(root, icon="key", variant="outline").pack(padx=10, pady=10)
+IconButton(root, icon="android2", color="success", variant="outline").pack(padx=10, pady=10)
+
+Button(root, text="Button", image=Icon("house", color="orange"), compound="left").pack(padx=10, pady=10)
+Button(root, text="Home", icon="house", variant="text").pack(padx=10, pady=10)
+
+CheckbuttonToggle(root, text="Verified", icon="fingerprint").pack(padx=10, pady=10)
+CheckbuttonToggle(root, text="Verified", icon="key").pack(padx=10, pady=10)
+
+Button(root, icon="android2", text="Button", variant="outline").pack(padx=10, pady=10)
+
+root.mainloop()
