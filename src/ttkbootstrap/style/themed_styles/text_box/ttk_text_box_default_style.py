76c5e5ccb6613a3ac0dd35a66121d2b03f591ae8
Israel Dryer <israel.dryer@gmail.com>
2025-05-27 03:08:56 -0400

    update style for inheritance

--- src/ttkbootstrap/style/themed_styles/text_box/ttk_text_box_default_style.py
+++ src/ttkbootstrap/style/themed_styles/text_box/ttk_text_box_default_style.py
@@ -14,29 +14,40 @@ class TTkTextBoxDefaultStyle(StyleBuilder):
     def __init__(self, theme: Theme):
         super().__init__(theme)
 
-    def invoke(self, color: str, **_):
+    def invoke(self, token: str, **extras):
         """Create the default input style"""
 
-        style = f'{color}.Input'
+        # check if the background color should be inherited from the parent
+        parent_background = extras.get('background', None)
+        container_bg = self.theme.background
+        if parent_background is not None and parent_background != container_bg:
+            style = f'{parent_background}.{token}.Input'  # inherited background style
+            container_bg = parent_background
+        else:
+            style = f'{token}.Input'
+
+        # check if style already exists
         if self.theme.has_style(style):
             return style
 
         # color token
-        color = "primary" if color == "default" else color
+        token = "primary" if token == "default" else token
 
         # button colors
-        foreground = self.theme.foreground
-        focused = self.theme.get_color(color)
-        border = self.theme.border
-        disabled = border
+        input_fg = self.theme.foreground
+        input_bg = self.theme.get_input_background()
+        border_focused = self.theme.get_color(token)
+        border_normal = self.theme.get_input_border_color()
+        input_disabled_bg = border_normal
 
+        # base image for input state
         base_border = load_asset_image('input.png')
 
         # state images
-        normal_img = self.theme.image_recolor_map(base_border, self.theme.background, border)
+        normal_img = self.theme.image_recolor_map(base_border, input_bg, border_normal)
         self.theme.register_asset(str(normal_img), normal_img)
 
-        focused_img = self.theme.image_recolor_map(base_border, self.theme.background, focused)
+        focused_img = self.theme.image_recolor_map(base_border, input_bg, border_focused)
         self.theme.register_asset(str(focused_img), focused_img)
 
         # Border element
@@ -58,14 +69,14 @@ class TTkTextBoxDefaultStyle(StyleBuilder):
         self.theme.configure(
             style,
             padding=(6, 4),
-            foreground=foreground,
-            background=self.theme.background,
+            foreground=input_fg,
+            background=container_bg,
             selectbackground=self.theme.secondary,
             selectforeground=self.theme.get_foreground('primary'),
-            insertcolor=foreground,
+            insertcolor=input_fg,
             insertwidth=2
         )
-        self.theme.map(style, foreground=[('disabled', disabled)])
+        self.theme.map(style, foreground=[('disabled', input_disabled_bg)])
 
         self.theme.add_style(style)
         return style
