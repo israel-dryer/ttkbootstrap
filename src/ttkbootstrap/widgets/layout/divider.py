2b89708e286e1689bb14d13728eca438c5168e9b
Israel Dryer <israel.dryer@gmail.com>
2025-06-03 21:41:27 -0400

    update mixins

--- src/ttkbootstrap/widgets/layout/divider.py
+++ src/ttkbootstrap/widgets/layout/divider.py
@@ -1,17 +1,18 @@
 from tkinter import Misc
 from tkinter.ttk import Separator as ttkSeparator
+from typing import Literal
 
-from ..mixins import BackgroundInheritMixin
+from ..mixins import BackgroundMixin, BaseMixin, OrientMixin, StyleMixin
 from ...ttk_types import StyleColor
-from ...style.styled_widget import StyledWidget
-
-try:
-    from typing import Literal, Unpack
-except ImportError:
-    from typing_extensions import Unpack
+from ...utils import keys_to_lower
 
 
-class Divider(BackgroundInheritMixin, StyledWidget, ttkSeparator):
+class Divider(
+    StyleMixin,
+    BaseMixin,
+    OrientMixin,
+    BackgroundMixin,
+):
 
     def __init__(
         self,
@@ -21,8 +22,25 @@ class Divider(BackgroundInheritMixin, StyledWidget, ttkSeparator):
         orient: Literal['horizontal', 'vertical'] = "horizontal",
         **kwargs,
     ):
+        kw = dict(kwargs)
+        self._master = master
+        self._kwargs = kw
         self._color = color
         self._variant = variant
+        self._orient = orient
         self._extras = {"orient": orient}
-        super().__init__(master, **kwargs)
-        self._init_style('divider', color=color, variant=self._variant, extras=self._extras, **kwargs)
+        self._inherit_background = kw.pop('inherit_background', False)
+        self._widget: "ttkSeparator"
+        self._render_widget()
+
+    def _render_widget(self):
+        """Create and initialize the internal ttk widget."""
+        self._widget: "ttkSeparator" = ttkSeparator(self._master, **keys_to_lower(self._kwargs))
+
+        self._initialize_style(
+            'divider',
+            color=self._color,
+            variant=self._variant,
+            extras=self._extras,
+            **self._kwargs
+        )
