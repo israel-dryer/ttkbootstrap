f3fae0dd5da2ee36bfba6b05e452d14c90b9cd14
Israel Dryer <israel.dryer@gmail.com>
2025-11-11 07:42:51 -0500

    update toggle style (checkbutton)

--- src/ttkbootstrap/style/builders/checkbutton.py
+++ src/ttkbootstrap/style/builders/checkbutton.py
@@ -94,3 +94,78 @@ def build_checkbutton_style(b: BootstyleBuilderBuilderTTk, ttk_style: str, color
 
     b.configure_style(ttk_style, background=background, foreground=foreground)
     b.map_style(ttk_style, background=[], foreground=[])
+
+
+@BootstyleBuilderBuilderTTk.register_builder('toggle', 'TCheckbutton')
+def build_toggle_style(b: BootstyleBuilderBuilderTTk, ttk_style: str, color: str = 'primary', **options):
+    accent_token = color or 'primary'
+    surface_token = options.get('surface_color', 'background')
+
+    background = b.color(surface_token)
+    background_hover = b.hover(background)
+    foreground = b.on_color(background)
+    foreground_disabled = b.disabled('text')
+
+    normal = b.color(accent_token)
+    foreground_active = b.on_color(normal)
+    pressed = b.active(normal)
+    hovered = b.hover(normal)
+    border = b.border(background)
+    focus = hovered
+    focus_ring = b.focus_ring(normal, background)
+    disabled = b.disabled()
+
+    normal_checked_img = recolor_image('switch-on', foreground_active, normal, background)
+    normal_unchecked_img = recolor_image('switch-off', background, border, background)
+
+    hovered_checked_img = recolor_image('switch-on', foreground_active, hovered, background)
+    hovered_unchecked_img = recolor_image('switch-off', background_hover, border, background)
+
+    pressed_checked_img = recolor_image('switch-on', foreground_active, pressed, background)
+    pressed_unchecked_img = recolor_image('switch-off', background_hover, pressed, background)
+
+    focus_checked_img = recolor_image('switch-on', foreground_active, focus, focus_ring)
+    focus_unchecked_img = recolor_image('switch-off', background_hover, focus, focus_ring)
+
+    disabled_checked_img = recolor_image('switch-on', disabled, foreground_disabled, background)
+    disabled_unchecked_img = recolor_image('switch-off', foreground_disabled, foreground_disabled, background)
+
+    spacer_img = create_transparent_image(8, 1)
+    b.create_style_element_image(ElementImage(f'{ttk_style}.spacer', spacer_img, sticky="ew"))
+
+    b.create_style_element_image(
+        ElementImage(f'{ttk_style}.indicator', normal_unchecked_img, sticky="ns", padding=3).state_specs(
+            [
+                # Disabled states
+                ('disabled selected', disabled_checked_img),
+                ('disabled !selected !alternate', disabled_unchecked_img),
+
+                # Focused states
+                ('focus selected', focus_checked_img),
+                ('focus !selected !alternate', focus_unchecked_img),
+
+                # Pressed states
+                ('pressed selected', pressed_checked_img),
+                ('pressed !selected !alternate', pressed_unchecked_img),
+
+                # Hover states
+                ('hover selected', hovered_checked_img),
+                ('hover !selected !alternate', hovered_unchecked_img),
+
+                # Normal base states
+                ('selected', normal_checked_img),
+                ('!selected !alternate', normal_unchecked_img),
+            ]
+        ))
+
+    b.create_style_layout(
+        ttk_style, Element('Checkbutton.padding', sticky="nsew").children(
+            [
+                Element(f'{ttk_style}.indicator', side="left", sticky=""),
+                Element(f'{ttk_style}.spacer', side="left"),
+                Element('Checkbutton.label', side="left", sticky="nsew")
+            ])
+    )
+
+    b.configure_style(ttk_style, background=background, foreground=foreground)
+    b.map_style(ttk_style, background=[], foreground=[])
