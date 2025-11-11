dc5369f3b0752fd07d3a0156665fd8d437374fbb
Israel Dryer <israel.dryer@gmail.com>
2025-11-11 16:45:59 -0500

    add icon support to label style.

--- src/ttkbootstrap/style/builders/label.py
+++ src/ttkbootstrap/style/builders/label.py
@@ -4,9 +4,20 @@ from ttkbootstrap.style.bootstyle_builder_ttk import BootstyleBuilderBuilderTTk
 
 
 @BootstyleBuilderBuilderTTk.register_builder('default', 'TLabel')
-def build_label(builder: BootstyleBuilderBuilderTTk, ttk_style: str, color: str = None, **options):
-    foreground = builder.color(color or "foreground")
+def build_label(b: BootstyleBuilderBuilderTTk, ttk_style: str, color: str = None, **options):
+    foreground = b.color(color or "foreground")
     surface_token = options.get('surface_color', 'background')
-    background = builder.color(surface_token)
+    background = b.color(surface_token)
 
-    builder.configure_style(ttk_style, background=background, foreground=foreground)
+    b.configure_style(ttk_style, background=background, foreground=foreground)
+
+    state_spec = dict(foreground=[('', foreground)])
+
+    # map icon if available
+    icon = options.get('icon')
+
+    if icon is not None:
+        icon = b.normalize_icon_spec(icon)
+        state_spec['image'] = b.map_stateful_icons(icon, state_spec['foreground'])
+
+    b.map_style(ttk_style, **state_spec)
