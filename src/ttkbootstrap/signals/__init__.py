734f59b16a0fc14840045365cb72fd66e862ba7a
Israel Dryer <42584742+israel-dryer@users.noreply.github.com>
2025-11-13 13:47:18 -0500

    Feature/implement signals for reactivity (#788)
    * implement reactive signals

---
+++ src/ttkbootstrap/signals/__init__.py
@@ -0,0 +1,7 @@
+from .signal import Signal
+from .types import TraceOperation
+
+__all__ = [
+    "Signal",
+    "TraceOperation",
+]
---
+++ src/ttkbootstrap/signals/types.py
@@ -0,0 +1,3 @@
+from typing import Literal
+
+TraceOperation = Literal["array", "read", "write", "unset"]
