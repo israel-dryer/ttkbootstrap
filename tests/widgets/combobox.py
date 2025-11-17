853b728a0499a70d72083f03603bef0662a56e9b
Israel Dryer <israel.dryer@appforia.net>
2025-11-16 19:44:29 -0500

    update test with theme changer.

--- tests/widgets/combobox.py
+++ tests/widgets/combobox.py
@@ -4,6 +4,14 @@ app = ttk.Window()
 
 colors = ['primary', 'secondary', 'success', 'info', 'warning', 'danger']
 
+def change_theme():
+    if app.style.theme_use() == 'dark':
+        app.style.theme_use('light')
+    else:
+        app.style.theme_use('dark')
+
+ttk.Button(app, text="Change Theme", command=change_theme).pack(pady=10)
+
 for color in colors:
     b = ttk.Combobox(app, bootstyle=color, width=20, values=colors)
     b.set(color)
