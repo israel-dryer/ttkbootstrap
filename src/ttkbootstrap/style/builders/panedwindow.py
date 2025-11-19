c51e2ac77e1a82d109252ddf68b2fb76292416b7
Israel Dryer <israel.dryer@gmail.com>
2025-11-18 21:46:44 -0500

    update paned window scaling and font; update paned window test.

--- src/ttkbootstrap/style/builders/panedwindow.py
+++ src/ttkbootstrap/style/builders/panedwindow.py
@@ -12,14 +12,13 @@ from ttkbootstrap.style.bootstyle_builder_ttk import BootstyleBuilderTTk
 def build_paned_window_style(b: BootstyleBuilderTTk, ttk_style: str, color: str = None, **options):
     accent_token = color or 'border'
     surface_token = options.get('surface_color', 'background')
-    sash_thickness = options.get('sash_thickness', 6)
+    sash_thickness = options.get('sash_thickness', b.scale(6))
 
     surface = b.color(surface_token)
     if accent_token == 'border':
         accent = b.border(surface)
     else:
         accent = b.color(accent_token)
-
 
     # Sash thickness is a global adjustment that affects all paned windows
     b.configure_style("Sash", sashthickness=sash_thickness, gripcount=0)
