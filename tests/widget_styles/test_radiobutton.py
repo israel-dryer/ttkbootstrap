ba6ff88643c064a80c43681273a8988408e000cb
Israel Dryer <israel.dryer@gmail.com>
2025-11-18 23:04:03 -0500

    update radiobutton scale; center is always surface color; update test.

--- tests/widget_styles/test_radiobutton.py
+++ tests/widget_styles/test_radiobutton.py
@@ -1,67 +1,64 @@
-import tkinter as tk
 import ttkbootstrap as ttk
-from random import choice
-from ttkbootstrap import utility
-utility.enable_high_dpi_awareness()
+from ttkbootstrap.constants import *
 
-DARK = 'superhero'
-LIGHT = 'flatly'
 
-def create_radiobutton_test(bootstyle, style, testname):
+def create_radiobutton_test(bootstyle, testname):
     frame = ttk.Frame(padding=10)
 
-    var = tk.Variable()
+    var = ttk.Variable()
 
     # title
-    title = ttk.Label(frame, text=testname, anchor=tk.CENTER)
-    title.pack(padx=5, pady=2, fill=tk.BOTH)
-    ttk.Separator(frame).pack(padx=5, pady=5, fill=tk.X)
+    title = ttk.Label(frame, text=testname, anchor=CENTER)
+    title.pack(padx=5, pady=2, fill=BOTH)
+    ttk.Separator(frame).pack(padx=5, pady=5, fill=X)
 
     # default style
     cb = ttk.Radiobutton(frame, text='default', bootstyle=bootstyle, value=0)
     cb.configure(variable=var)
-    cb.pack(padx=5, pady=5, fill=tk.BOTH)
+    cb.pack(padx=5, pady=5, fill=BOTH)
     cb.invoke()
 
     # color styles
-    for i, color in enumerate(style.colors):
+    for i, color in enumerate(['primary', 'secondary', 'success', 'info', 'warning', 'danger']):
         cb = ttk.Radiobutton(frame, text=color, bootstyle=f'{color}-{bootstyle}')
-        cb.configure(variable=var, value=i+1)
-        cb.pack(padx=5, pady=5, fill=tk.BOTH)
+        cb.configure(variable=var, value=i + 1)
+        cb.pack(padx=5, pady=5, fill=BOTH)
         cb.invoke()
 
     # disabled style
     cb = ttk.Radiobutton(
-        master=frame, 
-        text=bootstyle, 
+        master=frame,
+        text=bootstyle,
         bootstyle=bootstyle,
-        state=tk.DISABLED,
+        state=DISABLED,
         variable=var,
-        value=i+1
+        value=i + 1
     )
-    cb.pack(padx=5, pady=5, fill=tk.BOTH)
+    cb.pack(padx=5, pady=5, fill=BOTH)
     cb.invoke()
 
     return frame
 
 
 def change_style():
-    theme = choice(style.theme_names())
-    style.theme_use(theme)    
+    if style.theme_use() == 'dark':
+        style.theme_use('light')
+    else:
+        style.theme_use('dark')
 
 
 if __name__ == '__main__':
     # create visual widget style tests
-    root = tk.Tk()
+    root = ttk.Window()
     style = ttk.Style()
 
     ttk.Button(text="Change Theme", command=change_style).pack(padx=10, pady=10)
 
-    test1 = create_radiobutton_test('', style, 'Radiobutton')
-    test1.pack(side=tk.LEFT, fill=tk.BOTH)
-    test4 = create_radiobutton_test('toolbutton', style, 'Toolbutton')
-    test4.pack(side=tk.LEFT, fill=tk.BOTH)
-    test5 = create_radiobutton_test('outline-toolbutton', style, 'Outline Toolbutton')
-    test5.pack(side=tk.LEFT, fill=tk.BOTH)
+    test1 = create_radiobutton_test('default', 'Radiobutton')
+    test1.pack(side=LEFT, fill=BOTH)
+    test4 = create_radiobutton_test('toolbutton', 'Toolbutton')
+    test4.pack(side=LEFT, fill=BOTH)
+    test5 = create_radiobutton_test('outline-toolbutton', 'Outline Toolbutton')
+    test5.pack(side=LEFT, fill=BOTH)
 
     root.mainloop()
