80ba82bb43e1e4dea25b3e65becbfa77d6d78a8a
Israel Dryer <israel.dryer@gmail.com>
2025-11-22 15:38:41 -0500

    update button width for demo.

--- tests/widgets/button.py
+++ tests/widgets/button.py
@@ -5,7 +5,7 @@ app = ttk.Window()
 colors = ['primary', 'secondary', 'success', 'info', 'warning', 'danger']
 
 for color in colors:
-    b = ttk.Button(app, text=color.title(), bootstyle=color, icon="plus-square", compound="left")
+    b = ttk.Button(app, text=color.title(), bootstyle=color, width=25, icon="bootstrap-fill", compound="left")
     b.pack(padx=20, pady=20)
 
 from tkinter import font
