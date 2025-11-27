23c33f786d503f914daa417b1e881102ab2dd7f9
Israel Dryer <israel.dryer@gmail.com>
2025-11-27 17:20:54 -0500

    implement the password entry #773 #818

--- src/ttkbootstrap/__init__.py
+++ src/ttkbootstrap/__init__.py
@@ -78,6 +78,7 @@ from ttkbootstrap.widgets.toast import Toast
 from ttkbootstrap.widgets.tooltip import ToolTip
 from ttkbootstrap.widgets.field import Field as Field
 from ttkbootstrap.widgets.textentry import TextEntry as TextEntry
+from ttkbootstrap.widgets.passwordentry import PasswordEntry as PasswordEntry
 
 # Import constants from widgets
 from ttkbootstrap.widgets import M, TTK_WIDGETS, TK_WIDGETS
@@ -116,6 +117,7 @@ __all__ = [
     'FloodGauge',
     'LabeledScale',
     'Meter',
+    'PasswordEntry',
     'Toast',
     'ToolTip',
     'TextEntry',
---
+++ tests/widgets/demo_password_entry.py
@@ -0,0 +1,10 @@
+import ttkbootstrap as ttk
+
+app = ttk.Window()
+
+
+pe = ttk.PasswordEntry()
+pe.pack(padx=10, pady=10, fill='x')
+
+
+app.mainloop()
