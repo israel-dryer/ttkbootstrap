7b47a8ace5293a464fb9b34160dc2b5dcbb378ad
Israel Dryer <israel.dryer@gmail.com>
2025-12-02 19:03:26 -0500

    implement the OptionMenu widget #860

---
+++ examples/demo_option_menu.py
@@ -0,0 +1,23 @@
+if __name__ == '__main__':
+    import ttkbootstrap as ttk
+
+    root = ttk.Window(themename="dark", size=(200, 200))
+
+    om = ttk.OptionMenu(
+        root, value="Python", dropdown_button_icon="chevron-down",
+        options=["Python", "Javascript", "Swift", "C#", "Go", "PHP", "Java"])
+    om.pack(pady=20, padx=20, fill='x')
+
+    om.on_changed(print)
+
+    om = ttk.OptionMenu(
+        root, value="Python",
+        state="disabled",
+        options=["Python", "Javascript", "Swift", "C#", "Go", "PHP", "Java"])
+    om.pack(pady=20, padx=20, fill='x')
+
+    om['show_dropdown_button'] = False
+
+
+
+    root.mainloop()
