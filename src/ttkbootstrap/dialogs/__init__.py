8a267c3a10de0a0d704d72fb9f93ceb1f8c9a2ec
Israel Dryer <israel.dryer@gmail.com>
2025-12-05 22:04:54 -0500

    implement filter dialog widget

--- src/ttkbootstrap/dialogs/__init__.py
+++ src/ttkbootstrap/dialogs/__init__.py
@@ -8,6 +8,7 @@ This package exposes Bootstrap-styled dialogs for common interactions:
 - ColorDropperDialog: pick a color from the screen with zoom preview
 - DateDialog: calendar date picker
 - FontDialog: font selection
+- FilterDialog: multi-select filter dialog with search
 
 Example:
 
@@ -28,6 +29,7 @@ from .colordropper import (
     ColorDropperDialog,
 )
 from .dialog import Dialog, DialogButton
+from .filterdialog import FilterDialog
 from .formdialog import FormDialog
 from .message import MessageDialog, MessageBox
 from .query import QueryDialog, QueryBox
@@ -38,6 +40,7 @@ __all__ = [
     # Base / core dialogs
     "Dialog",
     "DialogButton",
+    "FilterDialog",
     "FormDialog",
     "MessageDialog",
     "QueryDialog",
