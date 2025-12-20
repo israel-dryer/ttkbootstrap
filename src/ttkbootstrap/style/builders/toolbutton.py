aea348d7d1d20b70a16b474631891345e620d3b0
Israel Dryer <israel.dryer@gmail.com>
2025-12-20 13:35:15 -0500

    fix calendar toolbutton style

--- src/ttkbootstrap/style/builders/toolbutton.py
+++ src/ttkbootstrap/style/builders/toolbutton.py
@@ -333,6 +333,84 @@ def build_text_toolbutton_style(b: BootstyleBuilderTTk, ttk_style: str, color: s
 
     b.map_style(ttk_style, **state_spec)
 
+
+@BootstyleBuilderTTk.register_builder('calendar_day', 'Toolbutton')
+def build_solid_toolbutton_style(b: BootstyleBuilderTTk, ttk_style: str, color: str = None, **options):
+    """
+    Configure the button style.
+
+    Style options include:
+        * icon_only
+    """
+    accent_token = color or 'primary'
+    surface_token = options.get('surface_color', 'background')
+
+    surface = b.color(surface_token)
+    on_surface = b.on_color(surface)
+    accent = b.color(accent_token)
+
+    active = b.active(accent)
+    accent_focus = b.focus(accent)
+    on_accent = b.on_color(accent)
+
+    selected = b.selected(accent)
+
+
+    focus_ring = b.focus_ring(accent_focus, surface)
+    disabled = b.disabled()
+    on_disabled = b.disabled('text', disabled)
+
+    normal_img = recolor_image('button', surface, surface, surface, surface)
+    normal_focus_img = recolor_image('button', surface, surface, focus_ring, surface)
+    active_img = recolor_image('button', active, active, surface, surface)
+    selected_img = recolor_image('button', selected, selected, surface, surface)
+    selected_focus_img = recolor_image('button', selected, selected, focus_ring, surface)
+
+    disabled_img = recolor_image('button', disabled, disabled, surface, surface)
+
+    b.create_style_element_image(
+        ElementImage(
+            f'{ttk_style}.border', normal_img, sticky="nsew", border=b.scale(8), padding=b.scale(8)).state_specs(
+            [
+                ('disabled', disabled_img),
+                ('pressed', selected_focus_img),
+                ('focus selected', selected_focus_img),
+                ('selected', selected_img),
+                ('focus !selected', normal_focus_img),
+                ('active !focus', active_img),
+                ('', normal_img)
+            ]))
+
+    b.create_style_layout(
+        ttk_style,
+        _toolbutton_layout(ttk_style),
+    )
+
+    button_padding = _toolbutton_padding(b, options)
+
+    b.configure_style(
+        ttk_style,
+        background=surface,
+        foreground=on_surface,
+        stipple="gray12",
+        relief='flat',
+        padding=button_padding,
+        anchor="center",
+        font="body"
+    )
+
+    state_spec = dict(
+        foreground=[('disabled', on_disabled), ('selected', on_accent), ('', on_surface)],
+    )
+
+    icon_only = options.get('icon_only', False)
+    default_size = b.scale(24) if icon_only else b.scale(20)
+    state_spec = _apply_icon_mapping(b, options, state_spec, default_size)
+
+    b.map_style(ttk_style, **state_spec)
+
+
+
 @BootstyleBuilderTTk.register_builder('calendar_range', 'Toolbutton')
 def build_solid_toolbutton_style(b: BootstyleBuilderTTk, ttk_style: str, color: str = None, **options):
     """
