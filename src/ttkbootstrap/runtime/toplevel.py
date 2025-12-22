c42011b4a92af47d99c8d405f6cf0e9b48981873
Israel Dryer <israel.dryer@gmail.com>
2025-12-21 23:51:04 -0500

    use google style docstrings.

--- src/ttkbootstrap/runtime/toplevel.py
+++ src/ttkbootstrap/runtime/toplevel.py
@@ -13,14 +13,9 @@ class Toplevel(BaseWindow, tkinter.Toplevel):
     methods, see the [tcl/tk documentation](https://tcl.tk/man/tcl8.6/TkCmd/toplevel.htm)
     and the [Python documentation](https://docs.python.org/3/library/tkinter.html#tkinter.Toplevel).
 
-    ![](../../assets/window/window-toplevel.png)
-
     Examples:
-
-        ```python
-        app = Toplevel(title="My Toplevel")
-        app.mainloop()
-        ```
+        >>> app = Toplevel(title="My Toplevel")
+        >>> app.mainloop()
     """
 
     def __init__(
@@ -40,79 +35,47 @@ class Toplevel(BaseWindow, tkinter.Toplevel):
             alpha: float = 1.0,
             **kwargs: Any,
     ) -> None:
-        """
-        Parameters:
+        """Initialize a Toplevel window.
 
-            title (str):
-                The title that appears on the application titlebar.
-
-            icon (tkinter.PhotoImage):
-                A PhotoImage or file path used for the titlebar icon.
+        Args:
+            title: The title that appears on the application titlebar.
+            icon: A PhotoImage or file path used for the titlebar icon.
                 If None, the default ttkbootstrap.png icon is used.
                 Internally this is passed to the `Toplevel.iconphoto` method.
-
-            size (tuple[int, int]):
-                The width and height of the application window.
-                Internally, this argument is passed to the
-                `Toplevel.geometry` method.
-
-            position (tuple[int, int]):
-                The horizontal and vertical position of the window on
+            size: The width and height of the application window.
+                Internally, this argument is passed to the `Toplevel.geometry` method.
+            position: The horizontal and vertical position of the window on
                 the screen relative to the top-left coordinate.
-                Internally this is passed to the `Toplevel.geometry`
-                method.
-
-            minsize (tuple[int, int]):
-                Specifies the minimum permissible dimensions for the
+                Internally this is passed to the `Toplevel.geometry` method.
+            minsize: Specifies the minimum permissible dimensions for the
                 window. Internally, this argument is passed to the
                 `Toplevel.minsize` method.
-
-            maxsize (tuple[int, int]):
-                Specifies the maximum permissible dimensions for the
+            maxsize: Specifies the maximum permissible dimensions for the
                 window. Internally, this argument is passed to the
                 `Toplevel.maxsize` method.
-
-            resizable (tuple[bool, bool]):
-                Specifies whether the user may interactively resize the
+            resizable: Specifies whether the user may interactively resize the
                 toplevel window. Must pass in two arguments that specify
-                this flag for _horizontal_ and _vertical_ dimensions.
+                this flag for horizontal and vertical dimensions.
                 This can be adjusted after the window is created by using
                 the `Toplevel.resizable` method.
-
-            transient (Union[Tk, Widget]):
-                Instructs the window manager that this widget is
+            transient: Instructs the window manager that this widget is
                 transient with regard to the widget master. Internally
                 this is passed to the `Toplevel.transient` method.
-
-            overrideredirect (bool):
-                Instructs the window manager to ignore this widget if
+            overrideredirect: Instructs the window manager to ignore this widget if
                 True. Internally, this argument is processed as
                 `Toplevel.overrideredirect(1)`.
-
-            windowtype (str):
-                On X11, requests that the window should be interpreted by
+            windowtype: On X11, requests that the window should be interpreted by
                 the window manager as being of the specified type. Internally,
                 this is passed to the `Toplevel.attributes('-type', windowtype)`.
-
-                See the [-type option](https://tcl.tk/man/tcl8.6/TkCmd/wm.htm#M64)
-                for a list of available options.
-
-            topmost (bool):
-                Specifies whether this is a topmost window (displays above all
+            topmost: Specifies whether this is a topmost window (displays above all
                 other windows). Internally, this processed by the window as
                 `Toplevel.attributes('-topmost', 1)`.
-
-            toolwindow (bool):
-                On Windows, specifies a toolwindow style. Internally, this is
+            toolwindow: On Windows, specifies a toolwindow style. Internally, this is
                 processed as `Toplevel.attributes('-toolwindow', 1)`.
-
-            alpha (float):
-                On Windows, specifies the alpha transparency level of the
+            alpha: On Windows, specifies the alpha transparency level of the
                 toplevel. Where not supported, alpha remains at 1.0. Internally,
                 this is processed as `Toplevel.attributes('-alpha', alpha)`.
-
-            **kwargs (Dict):
-                Other optional keyword arguments.
+            **kwargs: Other optional keyword arguments.
         """
         # Extract iconify kwarg if present
         iconify = kwargs.pop('iconify', None)
