57b49bcf2a8808352246350d5ed7ed69219f75a4
Israel Dryer <israel.dryer@gmail.com>
2025-12-27 21:21:24 -0500

    update default selection background.

--- src/ttkbootstrap/style/builders/treeview.py
+++ src/ttkbootstrap/style/builders/treeview.py
@@ -29,7 +29,6 @@ def build_tree_style(b: BootstyleBuilderTTk, ttk_style: str, color: str = None, 
         * header_background
     """
     surface_token = options.get('surface_color', 'background')
-
     surface = b.color(surface_token)
 
     if options.get('show_border', True):
@@ -52,14 +51,8 @@ def build_tree_style(b: BootstyleBuilderTTk, ttk_style: str, color: str = None, 
     on_surface_disabled = b.disabled('text', surface)
     hover = b.active(surface)
 
-    if options.get('select_background'):
-        select_background = b.color(options.get('select_background'))
-    else:
-        if color in TOKENS:
-            select_background = b.color(f"{color}[subtle]")
-        else:
-            select_background = b.color('primary[subtle]')
-
+    select_background_token = options.get('select_background', 'primary')
+    select_background = b.color(select_background_token)
     select_hover = b.active(select_background)
     on_select = b.on_color(select_background)
 
