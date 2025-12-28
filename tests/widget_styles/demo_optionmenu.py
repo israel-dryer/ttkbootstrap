19e0664fb936a19e6e4c3f5d107ca5823c053a04
Israel Dryer <israel.dryer@gmail.com>
2025-12-27 19:57:35 -0500

    update option menu test

---
+++ tests/widget_styles/demo_optionmenu.py
@@ -0,0 +1,19 @@
+import ttkbootstrap as ttk
+
+if __name__ == '__main__':
+    # create visual widget style tests
+    root = ttk.App()
+
+    ttk.Button(text="Change Theme", command=ttk.toggle_theme).pack(padx=10, pady=10)
+    var = ttk.Variable()
+    om = ttk.OptionMenu(root, var, ['dark', 'light'])
+    om.pack(padx=10, pady=10, fill='x')
+
+    for i, color in enumerate(['primary', 'secondary', 'success', 'info', 'warning', 'danger']):
+        var = ttk.Variable()
+        om = ttk.OptionMenu(root, var, ['primary', 'secondary', 'success', 'info', 'warning', 'danger'], bootstyle=color)
+        om.pack(padx=10, pady=10, fill='x')
+
+    root.mainloop()
+
+
