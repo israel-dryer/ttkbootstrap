a11539b02903137f85c9f7b42326908854efd910
Israel Dryer <israel.dryer@gmail.com>
2026-01-03 11:33:22 -0500

    improve sampling for cleaner crisper lines.

--- src/ttkbootstrap/style/builders/frame.py
+++ src/ttkbootstrap/style/builders/frame.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from ttkbootstrap.style.bootstyle_builder_ttk import BootstyleBuilderTTk
 from ttkbootstrap.style.element import Element, ElementImage
-from ttkbootstrap.style.utility import recolor_image
+from ttkbootstrap.style.utility import create_rounded_border_image
 
 
 @BootstyleBuilderTTk.register_builder('default', 'TFrame')
@@ -10,16 +10,28 @@ def build_frame(b: BootstyleBuilderTTk, ttk_style: str, accent: str = None, **op
     surface_token = options.get('surface') or 'background'
     show_border = options.get('show_border', False)
     surface = b.color(surface_token)
-    border = b.border(surface)
+    stroke = b.border(surface)
+    stroke_thickness = options.get('stroke_thickness', 1)
+    stroke_radius = options.get('stroke_radius', 4)
+
 
     # border assets and styles
     if show_border:
-        border_img = recolor_image('border', surface, border, surface, surface)
+        border_img = create_rounded_border_image(
+            size=100,
+            radius=stroke_radius,
+            fill=surface,
+            stroke=stroke,
+            thickness=stroke_thickness
+        )
+
+        # border_img = recolor_image('border', surface, stroke, surface, surface)
         b.create_style_element_image(
             ElementImage(
                 f'{ttk_style}.border',
                 border_img,
-                border=b.scale(6),
+                border=int(stroke_radius * 4),
+                padding=int(stroke_radius * 4),
                 sticky="nsew")
         )
         b.create_style_layout(ttk_style, Element(f'{ttk_style}.border', sticky="nsew"))
