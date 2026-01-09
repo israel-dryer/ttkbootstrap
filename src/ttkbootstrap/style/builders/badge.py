99f4b8f9b7b4acc449ea07faed0ee265706a1c0a
Israel Dryer <israel.dryer@gmail.com>
2026-01-09 15:54:43 -0500

    badge: update assets, variant, and stylebuilder

--- src/ttkbootstrap/style/builders/badge.py
+++ src/ttkbootstrap/style/builders/badge.py
@@ -2,49 +2,43 @@
 
 This module contains style builders for ttk.Label widget badge variants.
 """
-
 from ttkbootstrap.style.bootstyle_builder_ttk import BootstyleBuilderTTk
 from ttkbootstrap.style.element import Element, ElementImage
-from ttkbootstrap.style.utility import recolor_image
+from ttkbootstrap.style.utility import recolor_element_image
 
 
 @BootstyleBuilderTTk.register_builder('square', 'TBadge')
-@BootstyleBuilderTTk.register_builder('default', 'TBadge')
 def build_default_badge_style(b: BootstyleBuilderTTk, ttk_style: str, accent: str = None, **options):
-    build_badge(b, ttk_style, accent, 'default', **options)
-
+    build_badge(b, ttk_style, accent, 'square', **options)
 
 @BootstyleBuilderTTk.register_builder('pill', 'TBadge')
 def build_pill_badge_style(b: BootstyleBuilderTTk, ttk_style: str, accent: str = None, **options):
     build_badge(b, ttk_style, accent, 'pill', **options)
 
 
-def build_badge(b: BootstyleBuilderTTk, ttk_style: str, accent: str = None, variant: str = 'default', **options):
-    surface_token = options.get('surface', 'content')
+def build_badge(b: BootstyleBuilderTTk, ttk_style: str, accent: str = None, variant: str = 'square', **options):
+    """Create a badge style for the variant specified"""
 
+    surface_token = options.get('surface', 'content')
     surface = b.color(surface_token)
     normal = b.color(accent or 'primary')
     foreground = b.on_color(normal)
 
-    normal_img = recolor_image(f'badge-{variant}', normal)
+    badge_img = recolor_element_image(f'badge_{variant}', normal, surface, surface, surface)
 
-    border = b.scale(6)
-    padding = b.scale((6, 0))
-
-    # button element
-    b.create_style_element_image(
-        ElementImage(
-            f'{ttk_style}.border', normal_img,
-            sticky="nsew", border=border, padding=padding))
+    b.create_style_element_image(ElementImage(f'{ttk_style}.border', badge_img.image, 
+                                              border=badge_img.meta.border, 
+                                              padding=badge_img.meta.padding, 
+                                              height=badge_img.meta.height,
+                                              sticky='nsew'))
 
     b.create_style_layout(
-        ttk_style, Element(f"{ttk_style}.border", sticky="").children(
+        ttk_style, Element(f"{ttk_style}.border", sticky="nsew").children(
             [
                 Element("Label.padding", sticky="nsew").children(
                     [
-                        Element("Label.label", sticky="nsew")
+                        Element("Label.label", sticky="ew")
                     ])
             ]))
-
-    style_padding = b.scale((4, 2))
-    b.configure_style(ttk_style, font="caption", background=surface, foreground=foreground, padding=style_padding)
+    
+    b.configure_style(ttk_style, background=surface, foreground=foreground, padding=(6, 0))
--- tests/widget_styles/style_badge.py
+++ tests/widget_styles/style_badge.py
@@ -2,7 +2,7 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 
 
-def create_label_style(bootstyle, test_name):
+def create_badge_style(variant, test_name):
     frame = ttk.Frame(root, padding=5)
 
     # title
@@ -11,13 +11,13 @@ def create_label_style(bootstyle, test_name):
     ttk.Separator(frame).pack(padx=5, pady=5, fill=X)
 
     # default
-    badge = ttk.Badge(frame, text='de', variant=bootstyle)
-    badge.pack(padx=5, pady=5, fill=BOTH)
+    badge = ttk.Badge(frame, text='default', variant=variant)
+    badge.pack(padx=5, pady=5)
 
     # colored
     for color in ['primary', 'secondary', 'success', 'info', 'warning', 'danger']:
-        lbl = ttk.Badge(frame, text=color, accent=color)
-        lbl.pack(padx=5, pady=5, fill=BOTH)
+        lbl = ttk.Badge(frame, text=color, accent=color, variant=variant, anchor='center')
+        lbl.pack(padx=5, pady=5)
 
     return frame
 
@@ -29,7 +29,7 @@ if __name__ == '__main__':
 
     ttk.Button(text="Change Theme", command=ttk.toggle_theme).pack(padx=10, pady=10)
 
-    create_label_style('default', 'Default').pack(side=LEFT)
-    create_label_style('pill', 'Pill').pack(side=LEFT)
+    create_badge_style('square', 'Square').pack(side=LEFT)
+    create_badge_style('pill', 'Pill').pack(side=LEFT)
 
     root.mainloop()
